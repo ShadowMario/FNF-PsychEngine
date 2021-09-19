a23ae33c78a0ae7ccf128f78c7f23db33c03bac6
YumDev12 <88625775+YumDev12@users.noreply.github.com>
2021-09-19 16:31:49 +0100

    Discord RPC Change
    Changed The Message When Your In The Menus

--- source/Discord.hx
+++ source/Discord.hx
@@ -36,7 +36,7 @@ class DiscordClient
 	static function onReady()
 	{
 		DiscordRpc.presence({
-			details: "In the Menus",
+			details: "In the Menus - Psych Engine",
 			state: null,
 			largeImageKey: 'icon',
 			largeImageText: "Psych Engine"
