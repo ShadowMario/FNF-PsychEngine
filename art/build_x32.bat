a1cb37b125f159cfa3a151bd5e497f1c5da17b0b
ShadowMario <44785097+ShadowMario@users.noreply.github.com>
2021-10-13 04:20:51 -0300

    Doing some tests
    - Fixed x32 builds not compiling properly unless you manually deleted
    - Working in an update warning thing

--- art/build_x32.bat
+++ art/build_x32.bat
@@ -2,9 +2,9 @@
 color 0a
 cd ..
 echo BUILDING GAME
-lime build windows -32 -release
+lime build windows -32 -release -D 32bits
 echo.
 echo done.
 pause
 pwd
-explorer.exe export\release\windows\bin
+explorer.exe export\32bit\windows\bin
