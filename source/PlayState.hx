3981d2d21e7bd20a09d0023d730c46e1f3e08713
MrClogsworthYT <85831572+MrClogsworthYT@users.noreply.github.com>
2021-10-14 15:07:43 +0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3883,13 +3883,13 @@ class PlayState extends MusicBeatState
 			filters.push(new ColorMatrixFilter(matrix));
 		}
 
-		if (OptionsSubState.Tritanopiabool)
+		if (OptionsState.Tritanopiabool)
 		{
 			var matrix:Array<Float> = [
-						0.20, 0.99, -.19, 0, 0,
-						0.16, 0.79, 0.04, 0, 0,
-						0.01, -.01,    1, 0, 0,
-						   0,    0,    0, 1, 0,
+						0.97, 0.11, -.08, 0, 0,
+						0.02, 0.82, 0.16, 0, 0,
+						0.06, 0.88, 0.18, 0, 0,
+					  	   0,    0,    0, 1, 0,
 					];
 			filters.push(new ColorMatrixFilter(matrix));
 		}
