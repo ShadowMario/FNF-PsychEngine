73368b5bf024a1ab5c2b8b67be505836287087ae
CyndaquilDAC <65635932+CyndaquilDAC@users.noreply.github.com>
2021-11-01 10:19:49 -0500

    Fix achievements menu not having a transition back to main menu state.

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -85,7 +85,7 @@ class AchievementsMenuState extends MusicBeatState
 
 		if (controls.BACK) {
 			FlxG.sound.play(Paths.sound('cancelMenu'));
-			FlxG.switchState(new MainMenuState());
+			MusicBeatState.switchState(new MainMenuState());
 		}
 	}
 
