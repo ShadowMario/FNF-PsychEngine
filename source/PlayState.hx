093a097e99ecc684e77fd8570d5a27f505fcc439
Stilic <lateman-jpeg@outlook.fr>
2021-11-23 00:23:35 +0100

    fixin pause input

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1991,7 +1991,7 @@ songSpeed = SONG.speed;
 		}
 		botplayTxt.visible = cpuControlled;
 
-		if (FlxG.keys.justPressed.ENTER && startedCountdown && canPause)
+		if (controls.PAUSE && startedCountdown && canPause)
 		{
 			var ret:Dynamic = callOnLuas('onPause', []);
 			if(ret != FunkinLua.Function_Stop) {
