fcc689bb93f6480d1b3b6b2070a87057452969ae
MeguminBOT <discord.meguminbot@gmail.com>
2021-11-22 16:59:03 +0100

    Change accuracy ratings to be realistic and not delusional
    70-79% is DEFINITELY NOT GOOD, THATS HORRIBLE LIKE WHAT THE HELL? You miss time more than 20% of a song and you still call that good? smh...

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -61,16 +61,15 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X = 42;
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
-	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
+	public static var ratingStuff:Array<Dynamic> = [ // Now actually tells the truth about your accuracy without being too harsh on your feelings. //Lulu
+		['Uninstall', 0.2], //From 0% to 20%
+		['Laughable', 0.75], //From 20% to 75%
+		['Horrible', 0.8], //From 75% to 80%
+		['Bad', 0.85], //From 80% to 85%
+		['OK', 0.9], //From 85% to 90%
+		['Decent', 0.95], //From 90% to 95%
+		['Good', 0.98], //From 95% to 98%
+		['Great', 1], //From 98% to 99%
 		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
