463493c0c9605cc953ec37a69580fd1416b3fee4
MeguminBOT <discord.meguminbot@gmail.com>
2021-12-03 01:34:46 +0100

    Add Sarvente's Mid-Fight Masses, but currently horrible (read commit)
    Charts are horrible, like they've always been in this mod.
    Animations are currently a bit janky and I will fix this in the future.
    Stages is just lazy ported, so no animations or anything.
    
    Rechart of these songs will be done in the future. But atleast the files are here now :)

---
+++ example_mods/stages/mfm-stage.lua
@@ -0,0 +1,10 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('mfm-stage', 'mfm-stage', -500, -500);
+	addLuaSprite('mfm-stage',false);
+
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+	end
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
