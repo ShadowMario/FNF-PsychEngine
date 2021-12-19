c6cea0af905ba224ffc311e355990e1aa08b1495
MeguminBOT <discord.meguminbot@gmail.com>
2021-12-19 17:59:47 +0100

    Update WeekData.hx defaults and Playstate.hx rating stuff

--- source/WeekData.hx
+++ source/WeekData.hx
@@ -48,14 +48,14 @@ class WeekData {
 	public static function createWeekFile():WeekFile {
 		var weekFile:WeekFile = {
 			songs: [["Bopeebo", "dad", [146, 113, 253]], ["Fresh", "dad", [146, 113, 253]], ["Dad Battle", "dad", [146, 113, 253]]],
-			weekCharacters: ['dad', 'bf', 'gf'],
-			weekBackground: 'stage',
-			weekBefore: 'tutorial',
+			weekCharacters: ['no_menu_character', 'no_menu_character', 'no_menu_character'],
+			weekBackground: 'no_menu_background',
+			weekBefore: 'week6-bsides',
 			storyName: 'Your New Week',
 			weekName: 'Custom Week',
 			freeplayColor: [146, 113, 253],
 			startUnlocked: true,
-			hideStoryMode: false,
+			hideStoryMode: true,
 			hideFreeplay: false,
 			difficulties: ''
 		};
