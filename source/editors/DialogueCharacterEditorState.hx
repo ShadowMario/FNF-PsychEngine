c8a7fbcf9e88a3e30aea227dc34ceddbc1ae258e
Aleksey <85291330+TheLeerName@users.noreply.github.com>
2021-12-20 01:38:00 +0700

    hide mouse after exiting dialogue portrait editor

--- source/editors/DialogueCharacterEditorState.hx
+++ source/editors/DialogueCharacterEditorState.hx
@@ -635,6 +635,7 @@ class DialogueCharacterEditorState extends MusicBeatState
 			if(FlxG.keys.justPressed.ESCAPE) {
 				MusicBeatState.switchState(new editors.MasterEditorMenu());
 				FlxG.sound.playMusic(Paths.music('freakyMenu'), 1);
+				FlxG.mouse.visible = false;
 				transitioning = true;
 			}
 
