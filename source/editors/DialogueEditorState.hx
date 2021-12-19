f97632f5060a42895710275a844337219ae1792c
Aleksey <85291330+TheLeerName@users.noreply.github.com>
2021-12-20 01:38:52 +0700

    hide mouse after exiting dialogue editor

--- source/editors/DialogueEditorState.hx
+++ source/editors/DialogueEditorState.hx
@@ -338,6 +338,7 @@ class DialogueEditorState extends MusicBeatState
 			if(FlxG.keys.justPressed.ESCAPE) {
 				MusicBeatState.switchState(new editors.MasterEditorMenu());
 				FlxG.sound.playMusic(Paths.music('freakyMenu'), 1);
+				FlxG.mouse.visible = false;
 				transitioning = true;
 			}
 			var negaMult:Array<Int> = [1, -1];
