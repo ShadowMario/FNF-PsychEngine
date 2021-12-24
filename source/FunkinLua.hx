f7bd7f2ba8701ce4c5987c7046c0a47d0ff93514
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-24 11:12:42 +0200

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1546,7 +1546,9 @@ class FunkinLua {
 			luaTrace('musicFadeOut is deprecated! Use soundFadeOut instead.', false, true);
 		});
 
+                #if desktop
 		Discord.DiscordClient.addLuaCallbacks(lua);
+                #end
 
 		call('onCreate', []);
 		#end
