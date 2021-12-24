dfec8b9e338ae053e1e79175c7dd3750c47e323f
Trayfellow <48515908+trayfellow@users.noreply.github.com>
2021-12-25 03:57:56 +0700

    yoloswag

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -90,6 +90,7 @@ class AchievementsMenuState extends MusicBeatState
 	}
 
 	function changeSelection(change:Int = 0) {
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
 		curSelected += change;
 		if (curSelected < 0)
 			curSelected = options.length - 1;
