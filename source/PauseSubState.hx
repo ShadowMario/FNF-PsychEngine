b64f3ecb823d831228fd0ef98736a16739bc324d
lemz1 <87707926+lemz1@users.noreply.github.com>
2021-12-27 17:37:54 +0100

    Bruh2

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -19,7 +19,7 @@ class PauseSubState extends MusicBeatSubstate
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
 	var menuItems:Array<String> = [];
-	var menuItemsOG:Array<String> = ['Resume', 'Restart Song', 'Change Difficulty','Exit to menu'];
+	var menuItemsOG:Array<String> = ['Resume', 'Restart Song', 'Change Difficulty', 'Exit to menu'];
 	var difficultyChoices = [];
 	var curSelected:Int = 0;
 
