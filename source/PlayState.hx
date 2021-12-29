39049ea8474e9927a5b43f67a421ae61c4586a9e
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-29 17:59:51 +0200

    To not freeze the game anymore

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1314,6 +1314,11 @@ class PlayState extends MusicBeatState
 			return;
 		} else {
 			FlxG.log.warn('Couldnt find video file: ' + fileName);
+                        if(endingSong) {
+		                endSong();
+		        } else {
+				startCountdown();
+			}
 		}
 		#end
 		if(endingSong) {
