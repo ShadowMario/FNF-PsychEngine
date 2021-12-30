e89e57bfe099380f47d10ff3c6d39446e30eecf5
CerBor <58470353+CerBor@users.noreply.github.com>
2021-12-30 21:54:43 +0700

    Added difficulty drop down

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -86,6 +86,8 @@ class ChartingState extends MusicBeatState
 	];
 
 	var _file:FileReference;
+	
+	var postfix:String = '';
 
 	var UI_box:FlxUITabMenu;
 
@@ -568,6 +570,27 @@ class ChartingState extends MusicBeatState
 		});
 		stageDropDown.selectedLabel = _song.stage;
 		blockPressWhileScrolling.push(stageDropDown);
+		
+		if (CoolUtil.difficulties[PlayState.storyDifficulty].toLowerCase() != 'normal')
+		{
+			postfix = '-' + CoolUtil.difficulties[PlayState.storyDifficulty].toLowerCase();
+		}
+
+		var difficultyDropDown = new FlxUIDropDownMenuCustom(stageDropDown.x, player3DropDown.y, FlxUIDropDownMenuCustom.makeStrIdLabelArray(CoolUtil.difficulties, true), function(difficulty:String)
+		{
+			var newDifficulty:String = CoolUtil.difficulties[Std.parseInt(difficulty)].toLowerCase();
+			trace("Current difficulty: " + CoolUtil.difficulties[PlayState.storyDifficulty]);
+			trace("New diffculty: " + newDifficulty);
+			PlayState.storyDifficulty = Std.parseInt(difficulty);
+			if (newDifficulty != 'normal')
+			{
+				postfix = '-' + newDifficulty;
+			}
+			PlayState.SONG = Song.loadFromJson(_song.song.toLowerCase() + postfix, _song.song.toLowerCase());
+			MusicBeatState.resetState();
+		});
+		difficultyDropDown.selectedLabel = CoolUtil.difficulties[PlayState.storyDifficulty];
+		blockPressWhileScrolling.push(difficultyDropDown);
 
 		var skin = PlayState.SONG.arrowSkin;
 		if(skin == null) skin = '';
@@ -604,12 +627,14 @@ class ChartingState extends MusicBeatState
 		tab_group_song.add(new FlxText(stepperSpeed.x, stepperSpeed.y - 15, 0, 'Song Speed:'));
 		tab_group_song.add(new FlxText(player2DropDown.x, player2DropDown.y - 15, 0, 'Opponent:'));
 		tab_group_song.add(new FlxText(player3DropDown.x, player3DropDown.y - 15, 0, 'Girlfriend:'));
+		tab_group_song.add(new FlxText(difficultyDropDown.x, difficultyDropDown.y - 15, 0, 'Difficulty:'));
 		tab_group_song.add(new FlxText(player1DropDown.x, player1DropDown.y - 15, 0, 'Boyfriend:'));
 		tab_group_song.add(new FlxText(stageDropDown.x, stageDropDown.y - 15, 0, 'Stage:'));
 		tab_group_song.add(new FlxText(noteSkinInputText.x, noteSkinInputText.y - 15, 0, 'Note Texture:'));
 		tab_group_song.add(new FlxText(noteSplashesInputText.x, noteSplashesInputText.y - 15, 0, 'Note Splashes Texture:'));
 		tab_group_song.add(player2DropDown);
 		tab_group_song.add(player3DropDown);
+		tab_group_song.add(difficultyDropDown);
 		tab_group_song.add(player1DropDown);
 		tab_group_song.add(stageDropDown);
 
@@ -2608,7 +2633,7 @@ class ChartingState extends MusicBeatState
 
 	function loadJson(song:String):Void
 	{
-		PlayState.SONG = Song.loadFromJson(song.toLowerCase(), song.toLowerCase());
+		PlayState.SONG = Song.loadFromJson(song.toLowerCase() + postfix, song.toLowerCase());
 		MusicBeatState.resetState();
 	}
 
@@ -2640,7 +2665,7 @@ class ChartingState extends MusicBeatState
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
 			_file.addEventListener(Event.CANCEL, onSaveCancel);
 			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
-			_file.save(data.trim(), Paths.formatToSongPath(_song.song) + ".json");
+			_file.save(data.trim(), Paths.formatToSongPath(_song.song) + postfix + ".json");
 		}
 	}
 	
