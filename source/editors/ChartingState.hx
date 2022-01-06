c68eff1c24a732478c6503512a61cf7a33d3042d
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2022-01-06 12:24:01 -0500

    Fixed bugs and new event
    Plus text!

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -73,8 +73,9 @@ class ChartingState extends MusicBeatState
 		['', "Nothing. Yep, that's right."],
 		['Hey!', "Plays the \"Hey!\" animation from Bopeebo,\nValue 1: BF = Only Boyfriend, GF = Only Girlfriend,\nSomething else = Both.\nValue 2: Custom animation duration,\nleave it blank for 0.6s"],
 		['Set GF Speed', "Sets GF head bopping speed,\nValue 1: 1 = Normal speed,\n2 = 1/2 speed, 4 = 1/4 speed etc.\nUsed on Fresh during the beatbox parts.\n\nWarning: Value must be integer!"],
-		['Blammed Lights', "Value 1: 0 = Turn off, 1 = Blue, 2 = Green,\n3 = Pink, 4 = Red, 5 = Orange, Anything else = Random.\n\nNote to modders: This effect is starting to get \nREEEEALLY overused, this isn't very creative bro smh."],
-		['Kill Henchmen', "For Mom's songs, don't use this please, i love them :("],
+		['Change Hurt Amount', "Changes the amount of damage Hurt Notes deal\nValue 1: Full notes (0.3 is default)\nValue 2: Sustain tail (0.1 is default)"],
+		['Blammed Lights', "Value 1: 0 = Turn off, 1 = Red, 2 = Black Anything else = Random"],
+		['Kill Henchmen', "For Mom's songs"],
 		['Add Camera Zoom', "Used on MILF on that one \"hard\" part\nValue 1: Camera zoom add (Default: 0.015)\nValue 2: UI zoom add (Default: 0.03)\nLeave the values blank if you want to use Default."],
 		['BG Freaks Expression', "Should be used only in \"school\" Stage!"],
 		['Trigger BG Ghouls', "Should be used only in \"schoolEvil\" Stage!"],
@@ -125,6 +126,7 @@ class ChartingState extends MusicBeatState
 
 	var gridBG:FlxSprite;
 	var gridMult:Int = 2;
+//	var gridSusness:Bool = true;
 
 	var daquantspot = 0;
 	var curEventSelected:Int = 0;
