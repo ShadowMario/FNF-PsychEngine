a74c447f962caaace234602f3924c82722bf3833
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2022-01-06 09:39:23 -0500

    Some development things

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,8 +24,8 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
+			"WARNING!\n
+			This has flashing lights!\n
 			Press ENTER to disable them now or go to Options Menu.\n
 			Press ESCAPE to ignore this message.\n
 			You've been warned!",
@@ -63,5 +63,7 @@ class FlashingState extends MusicBeatState
 			}
 		}
 		super.update(elapsed);
+		// if update = trace("Among sus") then
+		// super.update(elapsed);
 	}
 }
