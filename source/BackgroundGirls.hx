fba4543f101fb168b97715d84eff335e5e7cd6df
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2022-01-06 10:47:56 -0500

    Changed code
    Removed some curse words

--- source/BackgroundGirls.hx
+++ source/BackgroundGirls.hx
@@ -23,12 +23,12 @@ class BackgroundGirls extends FlxSprite
 	public function swapDanceType():Void
 	{
 		isPissed = !isPissed;
-		if(!isPissed) { //Gets unpissed
+		if(!isPissed) { //Gets uncats
 			animation.addByIndices('danceLeft', 'BG girls group', CoolUtil.numberArray(14), "", 24, false);
 			animation.addByIndices('danceRight', 'BG girls group', CoolUtil.numberArray(30, 15), "", 24, false);
-		} else { //Pisses
-			animation.addByIndices('danceLeft', 'BG fangirls dissuaded', CoolUtil.numberArray(14), "", 24, false);
-			animation.addByIndices('danceRight', 'BG fangirls dissuaded', CoolUtil.numberArray(30, 15), "", 24, false);
+		} else { //Cats
+			animation.addByIndices('danceLeft', 'BG girls group', CoolUtil.numberArray(14), "", 24, false);
+			animation.addByIndices('danceRight', 'BG girls group', CoolUtil.numberArray(30, 15), "", 24, false)
 		}
 		dance();
 	}
