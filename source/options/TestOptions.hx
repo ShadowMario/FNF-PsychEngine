7f821cb0efb0da182c00c95abebff14d2dec621c
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2022-01-07 13:59:13 -0500

    Test Options
    Testing purps

---
+++ source/options/TestOptions.hx
@@ -0,0 +1,46 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class TestOptions extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'TestOptions';
+		rpcTitle = 'Test settings menu'; //for Discord Rich Presence
+		
+		var option:Option = new Option('Test',
+		'Testing',
+		'fpsRainbow',
+		'bool',
+		false);
+		addOption(option);
+
+		super();
+	}
+}
