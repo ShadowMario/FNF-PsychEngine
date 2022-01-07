4306969515450ffd81aa4f859d86ef97653f0871
MeguminBOT <discord.meguminbot@gmail.com>
2022-01-07 04:31:54 +0100

    osu! converts now have their own stagebg and no characters.
    Remove boyfriend and opponent from osu! converts, also remove gf but not the speaker itself.
    custom stage made from week1 for osu! converts.

---
+++ example_mods/stages/osu-stage.lua
@@ -0,0 +1,33 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('osu-stageback', 'osu-stageback', -600, -300);
+	setScrollFactor('stageback', 0.9, 0.9);
+	
+	makeLuaSprite('stagefront', 'stagefront', -650, 600);
+	setScrollFactor('stagefront', 0.9, 0.9);
+	scaleObject('stagefront', 1.1, 1.1);
+
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+		makeLuaSprite('stagelight_left', 'stage_light', -125, -100);
+		setScrollFactor('stagelight_left', 0.9, 0.9);
+		scaleObject('stagelight_left', 1.1, 1.1);
+		
+		makeLuaSprite('stagelight_right', 'stage_light', 1225, -100);
+		setScrollFactor('stagelight_right', 0.9, 0.9);
+		scaleObject('stagelight_right', 1.1, 1.1);
+		setProperty('stagelight_right.flipX', true); --mirror sprite horizontally
+
+		makeLuaSprite('stagecurtains', 'stagecurtains', -500, -300);
+		setScrollFactor('stagecurtains', 1.3, 1.3);
+		scaleObject('stagecurtains', 0.9, 0.9);
+	end
+
+	addLuaSprite('osu-stageback', false);
+	addLuaSprite('stagefront', false);
+	addLuaSprite('stagelight_left', false);
+	addLuaSprite('stagelight_right', false);
+	addLuaSprite('stagecurtains', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
