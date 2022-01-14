77c0e5c0d76204f5b1568396a3ddd26771af4ff2
tposejank <81495861+tposejank@users.noreply.github.com>
2022-01-14 14:41:30 -0500

    fuck

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -313,7 +313,7 @@ class AchievementObject extends FlxSpriteGroup {
 		achievementBG.scrollFactor.set();
 
 		var imagePath = Paths.image('achievementgrid');
-		var modsImage;
+		var modsImage = null;
 		var isModIcon:Bool = false;
 
 		if (Achievements.loadedAchievements.exists(name)) {
