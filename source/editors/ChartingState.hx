23d16e82a75fcd8fd045b12f6cd0e56f7fd9e621
bbpanzu <45317642+bbpanzu@users.noreply.github.com>
2022-01-19 16:17:10 -0500

    chart reloading on current diffs

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -2629,7 +2629,13 @@ class ChartingState extends MusicBeatState
 
 	function loadJson(song:String):Void
 	{
+		//make it look sexier if possible
+		if (CoolUtil.difficulties[PlayState.storyDifficulty] != "Normal"){
+		PlayState.SONG = Song.loadFromJson(song.toLowerCase()+"-"+CoolUtil.difficulties[PlayState.storyDifficulty], song.toLowerCase());
+			
+		}else{
 		PlayState.SONG = Song.loadFromJson(song.toLowerCase(), song.toLowerCase());
+		}
 		MusicBeatState.resetState();
 	}
 
