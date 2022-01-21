988665537b9ad60ea4c9020ea6ff16a5239dc6c6
jmxamongusmodder <80003646+jmxamongusmodder@users.noreply.github.com>
2022-01-21 08:27:44 -0500

    bug fix

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -64,6 +64,13 @@ class AchievementsMenuState extends MusicBeatState
 			add(icon);
 		}
 
+		descBox = new FlxSprite().makeGraphic(1, 1, FlxColor.BLACK);
+		descBox.alpha = 0.5;
+		descBox.setPosition(descText.x - 10, descText.y - 10);
+		descBox.setGraphicSize(Std.int(descText.width + 20), Std.int(descText.height + 25));
+		descBox.updateHitbox();
+                add(descBox);
+
 		descText = new FlxText(150, 600, 980, "", 32);
 		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		descText.scrollFactor.set();
@@ -72,13 +79,6 @@ class AchievementsMenuState extends MusicBeatState
 		descText.y += 270;
 		add(descText);
 		changeSelection();
-                
-		descBox = new FlxSprite().makeGraphic(1, 1, FlxColor.BLACK);
-		descBox.alpha = 0.6;
-		descBox.setPosition(descText.x - 10, descText.y - 10);
-		descBox.setGraphicSize(Std.int(descText.width + 20), Std.int(descText.height + 25));
-		descBox.updateHitbox();
-                add(descBox);
 
 		super.create();
 	}
