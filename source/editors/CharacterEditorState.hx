40499a0cce807c6fe505fa34b7e4a991a3f60ec8
blueDev2 <luckywill339@gmail.com>
2022-01-21 20:34:02 -0500

    Clicking UI boxes do not start drag and drop

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -64,7 +64,7 @@ class CharacterEditorState extends MusicBeatState
 		this.daAnim = daAnim;
 		this.goToPlayState = goToPlayState;
 	}
-
+	
 	var UI_box:FlxUITabMenu;
 	var UI_characterbox:FlxUITabMenu;
 
@@ -153,7 +153,8 @@ class CharacterEditorState extends MusicBeatState
 			\nSpace - Play Animation
 			\nArrow Keys - Move Character Offset
 			\nR - Reset Current Offset
-			\nHold Shift to Move 10x faster\n", 12);
+			\nHold Shift to Move 10x faster
+			\n", 12);
 		tipText.cameras = [camHUD];
 		tipText.setFormat(null, 12, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		tipText.scrollFactor.set();
@@ -1195,28 +1196,30 @@ class CharacterEditorState extends MusicBeatState
 
 
 				var mouseLoc = FlxG.mouse.getPosition();
-				if(FlxG.mouse.justPressed)
+				if(!FlxG.mouse.overlaps(UI_box) && !FlxG.mouse.overlaps(UI_characterbox))
 				{
-					mouseLocation = mouseLoc;
-				}
-				else if(FlxG.mouse.pressed && FlxG.mouse.justMoved)
-				{
-					var xDiff:Int = Std.int(mouseLoc.x - mouseLocation.x);
-					var yDiff:Int = Std.int(mouseLoc.y - mouseLocation.y);
-
-					char.animationsArray[curAnim].offsets[0] -= xDiff;
-					char.animationsArray[curAnim].offsets[1] -= yDiff;
-					char.addOffset(char.animationsArray[curAnim].anim, char.animationsArray[curAnim].offsets[0], char.animationsArray[curAnim].offsets[1]);
-					ghostChar.addOffset(char.animationsArray[curAnim].anim, char.animationsArray[curAnim].offsets[0], char.animationsArray[curAnim].offsets[1]);
-					
-					char.playAnim(char.animationsArray[curAnim].anim, false);
-					if(ghostChar.animation.curAnim != null && char.animation.curAnim != null && char.animation.curAnim.name == ghostChar.animation.curAnim.name) {
-						ghostChar.playAnim(char.animation.curAnim.name, false);
+					if(FlxG.mouse.justPressed)
+					{
+						mouseLocation = mouseLoc;
 					}
-					genBoyOffsets();
-					mouseLocation = mouseLoc;
-				}
+					else if(FlxG.mouse.pressed && FlxG.mouse.justMoved)
+					{
+						var xDiff:Int = Std.int(mouseLoc.x - mouseLocation.x);
+						var yDiff:Int = Std.int(mouseLoc.y - mouseLocation.y);
 
+						char.animationsArray[curAnim].offsets[0] -= xDiff;
+						char.animationsArray[curAnim].offsets[1] -= yDiff;
+						char.addOffset(char.animationsArray[curAnim].anim, char.animationsArray[curAnim].offsets[0], char.animationsArray[curAnim].offsets[1]);
+						ghostChar.addOffset(char.animationsArray[curAnim].anim, char.animationsArray[curAnim].offsets[0], char.animationsArray[curAnim].offsets[1]);
+						
+						char.playAnim(char.animationsArray[curAnim].anim, false);
+						if(ghostChar.animation.curAnim != null && char.animation.curAnim != null && char.animation.curAnim.name == ghostChar.animation.curAnim.name) {
+							ghostChar.playAnim(char.animation.curAnim.name, false);
+						}
+						genBoyOffsets();
+						mouseLocation = mouseLoc;
+					}
+				}
 				
 				var controlArray:Array<Bool> = [FlxG.keys.justPressed.LEFT, FlxG.keys.justPressed.RIGHT, FlxG.keys.justPressed.UP, FlxG.keys.justPressed.DOWN];				
 				for (i in 0...controlArray.length) {
