a4ca1cbc4f287cfecc9580bebefdc7e3406a2f34
CerBor <58470353+CerBor@users.noreply.github.com>
2022-01-25 22:04:58 +0700

    Whoooooops...
    Forgot about this

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -2687,8 +2687,8 @@ class ChartingState extends MusicBeatState
 	function loadJson(song:String):Void
 	{
 		//make it look sexier if possible
-		if (CoolUtil.difficulties[PlayState.storyDifficulty] != "Normal"){
-		PlayState.SONG = Song.loadFromJson(song.toLowerCase()+"-"+CoolUtil.difficulties[PlayState.storyDifficulty], song.toLowerCase());
+		if (CoolUtil.defaultDifficulties[PlayState.storyDifficulty] != "Normal"){
+		PlayState.SONG = Song.loadFromJson(song.toLowerCase()+"-"+CoolUtil.defaultDifficulties[PlayState.storyDifficulty], song.toLowerCase());
 			
 		}else{
 		PlayState.SONG = Song.loadFromJson(song.toLowerCase(), song.toLowerCase());
