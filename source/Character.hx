ee308cdee3bb2250218138f4c86e283fb1ae10ee
lemz1 <87707926+lemz1@users.noreply.github.com>
2022-02-21 18:59:16 +0100

    crash doesn't happen anymore when opening 'adjust delay and combo'

--- source/Character.hx
+++ source/Character.hx
@@ -287,7 +287,7 @@ class Character extends FlxSprite
 				dance();
 			}
 
-			if (!isPlayer && !PlayState.opponentChart || PlayState.instance.cpuControlled)
+			if (!isPlayer && !PlayState.opponentChart || PlayState.instance.cpuControlled && PlayState.opponentChart)
 			{
 				if (animation.curAnim.name.startsWith('sing'))
 				{
