23bd0ee6c7309799ef86dd2a707e962b27f817e4
PrismLight <97522785+PrismLight@users.noreply.github.com>
2022-02-22 08:20:33 -0500

    Better Health Bar For People With Custom Bars

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1203,9 +1203,8 @@ class PlayState extends MusicBeatState
 	}
 
 	public function reloadHealthBarColors() {
-		healthBar.createFilledBar(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
-			FlxColor.fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
-			
+		healthBar.createImageEmptyBar(Paths.image('healthBar'), FlxColor.(fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2])));
+		healthBar.createImageFilledBar(Paths.image('healthBar'), FlxColor.(fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2])));
 		healthBar.updateBar();
 	}
 
