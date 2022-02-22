8d77b4a2648d27512cd65995bbd2ac27a639dc3a
Gui iago∞ <45212377+Gui-iago@users.noreply.github.com>
2022-02-22 02:26:43 -0400

    fixes and removal of things that shouldn't be added to this PR

--- source/editors/EditorPlayState.hx
+++ source/editors/EditorPlayState.hx
@@ -194,18 +194,7 @@ class EditorPlayState extends MusicBeatState
 
 		var songData = PlayState.SONG;
 		Conductor.mapBPMChanges(songData);
-		if (songData.notes[Math.floor(curStep / 16)].changeBPM && songData.notes[Math.floor(curStep / 16)].bpm > 0)
-		{
-			Conductor.changeBPM(songData.notes[Math.floor(curStep / 16)].bpm);
-		}
-		else
-		{
-			var daBPM:Float = songData.bpm;
-			for (i in 0...Math.floor(curStep / 16))
-				if (songData.notes[i].changeBPM)
-					daBPM = songData.notes[i].bpm;
-			Conductor.changeBPM(daBPM);
-		}
+		Conductor.changeBPM(songData.bpm);
 		
 		notes = new FlxTypedGroup<Note>();
 		add(notes);
@@ -526,11 +515,6 @@ class EditorPlayState extends MusicBeatState
 		if (generatedMusic)
 		{
 			notes.sort(FlxSort.byY, ClientPrefs.downScroll ? FlxSort.ASCENDING : FlxSort.DESCENDING);
-		}
-
-		if (PlayState.SONG.notes[Math.floor(curStep / 16)] != null && PlayState.SONG.notes[Math.floor(curStep / 16)].changeBPM)
-		{
-			Conductor.changeBPM(PlayState.SONG.notes[Math.floor(curStep / 16)].bpm);
 		}
 	}
 
