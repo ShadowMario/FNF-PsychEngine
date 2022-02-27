72c0d06cd9ecd835472c5a02bfc28278075e08b2
frantastic24 <95725102+frantastic24@users.noreply.github.com>
2022-02-27 19:53:02 -0300

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -90,9 +90,6 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
 		optionsArray.push(option);
-		
-		var option:GameplayOption = new GameplayOption('Play Opponents Chart', 'opponentplay', 'bool', false);
-		optionsArray.push(option);
 	}
 
 	public function getOptionByName(name:String)
