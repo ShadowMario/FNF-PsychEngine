ac3b267ca9d568c6af482deeddcdc38a6465b232
ShadowMario <44785097+ShadowMario@users.noreply.github.com>
2022-03-03 22:01:13 -0300

    Doing things

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -556,6 +556,7 @@ class CharacterEditorState extends MusicBeatState
 				char.antialiasing = true;
 			}
 			char.noAntialiasing = noAntialiasingCheckBox.checked;
+			ghostChar.antialiasing = char.antialiasing;
 		};
 
 		positionXStepper = new FlxUINumericStepper(flipXCheckBox.x + 110, flipXCheckBox.y, 10, char.positionArray[0], -9000, 9000, 0);
@@ -1031,6 +1032,7 @@ class CharacterEditorState extends MusicBeatState
 			char.alpha = 1;
 		}
 		ghostChar.color = 0xFF666688;
+		ghostChar.antialiasing = char.antialiasing;
 		
 		ghostChar.setGraphicSize(Std.int(ghostChar.width * char.jsonScale));
 		ghostChar.updateHitbox();
