262479bb4b020fa76c2785ef1e4b86e78f37d226
WestCraft15 <westonebright@yahoo.com>
2022-03-08 08:11:29 -0800

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -70,11 +70,6 @@ class GameOverSubstate extends MusicBeatSubstate
 		camFollowPos = new FlxObject(0, 0, 1, 1);
 		camFollowPos.setPosition(FlxG.camera.scroll.x + (FlxG.camera.width / 2), FlxG.camera.scroll.y + (FlxG.camera.height / 2));
 		add(camFollowPos);
-
-		if (ClientPrefs.instantRespawn) 
-		{
-			MusicBeatState.resetState();
-		}
 	}
 
 	var isFollowingAlready:Bool = false;
