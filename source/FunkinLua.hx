1af7e41753bb41dbe1f83ce282dcca215ceaa7c0
Stilic <lateman-jpeg@outlook.fr>
2022-03-13 10:07:13 +0100

    add state lua shit

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -380,6 +380,20 @@ class FunkinLua {
 			return Reflect.setProperty(Type.resolveClass(classVar), variable, value);
 		});
 
+                //state shit for trolling coming??
+                Lua_helper.add_callback(lua, "switchState", function(state:String) {
+                        var bigPiss:Dynamic = Type.resolveClass(state);
+                        if (bigPiss != null)
+                        {
+			        MusicBeatState.switchState(bigPiss);
+                        }
+                        else
+                        {
+                                luaTrace("State " + state + " doesn't exist!");
+                        }
+		});
+                Lua_helper.add_callback(lua, "resetState", MusicBeatState.resetState);
+
 		//shitass stuff for epic coders like me B)  *image of obama giving himself a medal*
 		Lua_helper.add_callback(lua, "getObjectOrder", function(obj:String) {
 			if(PlayState.instance.modchartSprites.exists(obj))
