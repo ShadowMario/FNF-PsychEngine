24fde6a1117c3452f300e31b0501a7bef4587b5e
jmxamongusmodder <80003646+jmxamongusmodder@users.noreply.github.com>
2022-03-13 13:03:49 -0400

    Shit #1

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -284,6 +284,12 @@ class FunkinLua {
 			Paths.clearUnusedMemory();
 			return true;
 		});
+		
+		Lua_helper.add_callback(lua, "setCurrentLevel", function(name:String) { //YOU CAN NOW CHANGE SONGS DURING A SONG WHICH I THINK WAS NEVER DONE BEFORE IN LUA!!
+			Paths.setCurrentLevel(name);
+//			Playstate.SONG = (name); // does this code work?? ill try it later.
+			return true;
+		});
 
 		Lua_helper.add_callback(lua, "loadGraphic", function(variable:String, image:String) {
 			var spr:FlxSprite = getObjectDirectly(variable);
