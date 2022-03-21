7fc71d8c1ad3913ec9d514a976c5bea2f581fb74
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2022-03-21 11:50:41 -0500

    stdint is case sensitive, windows is not used

--- source/vlc/cpp/src/LibVLC.cpp
+++ source/vlc/cpp/src/LibVLC.cpp
@@ -1,8 +1,8 @@
 #include <mutex>
 #include <iostream>
 #include <string>
-#include <StdInt.h>
-#include <windows.h> 
+#include <stdint.h>
+//#include <windows.h>
 
 using std::string;
 using namespace std;
