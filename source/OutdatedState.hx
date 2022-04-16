952bbaeec4047b6ae421f72db7ac6d7c24bc7f6a
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-16 19:05:25 +0300

    Checking for updates 😎

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -45,7 +45,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
+				CoolUtil.browserLoad("https://github.com/jigsaw-4277821/FNF-PsychEngine-Android-Support/actions");
 			}
 			else if(controls.BACK) {
 				leftState = true;
