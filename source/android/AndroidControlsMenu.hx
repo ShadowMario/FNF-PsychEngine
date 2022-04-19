a76d1e9b7d5531a50390eb897330cb8ecca6c5fe
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-19 09:54:16 +0300

    fixing errors

--- source/android/AndroidControlsMenu.hx
+++ source/android/AndroidControlsMenu.hx
@@ -20,7 +20,7 @@ class AndroidControlsMenu extends MusicBeatState
 	var downPozition:FlxText;
 	var leftPozition:FlxText;
 	var rightPozition:FlxText;
-	var inputvari:Alphabet;
+	var inputvari:PsychAlphabet;
 	var leftArrow:FlxSprite;
 	var rightArrow:FlxSprite;
 	var controlitems:Array<String> = ['Pad-Right','Pad-Left','Pad-Custom','Duo','Hitbox','Keyboard'];
@@ -41,7 +41,7 @@ class AndroidControlsMenu extends MusicBeatState
 		bg.color = FlxColor.fromHSB(FlxG.random.int(0, 359), FlxG.random.float(0, 0.8), FlxG.random.float(0.3, 1));
 		add(bg);
 
-		var titleText:Alphabet = new Alphabet(0, 0, "Android Controls", true, false, 0, 0.6);
+		var titleText:PsychAlphabet = new PsychAlphabet(0, 0, "Android Controls", true, false, 0, 0.6);
 		titleText.x += 60;
 		titleText.y += 40;
 		titleText.alpha = 0.4;
@@ -55,7 +55,7 @@ class AndroidControlsMenu extends MusicBeatState
 		hbox.visible = false;
 		add(hbox);
 
-		inputvari = new Alphabet(0, 50, controlitems[curSelected], false, false, 0.05, 0.8);
+		inputvari = new PsychAlphabet(0, 50, controlitems[curSelected], false, false, 0.05, 0.8);
 		inputvari.screenCenter(X);
 		add(inputvari);
 
