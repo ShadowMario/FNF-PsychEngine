0dda3006bada4248b1155dbf09ba7a64f8305f52
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-19 09:43:24 +0300

    Blue Screen Reference

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -123,7 +123,7 @@ class SUtil
         Sys.println("Crash dump saved in " + Path.normalize(path));
         Sys.println("Making a simple alert ...");
 
-        SUtil.applicationAlert("Uncaught Error, The Call Stack: ", errMsg);
+        SUtil.applicationAlert("Uncaught Error :(, The Call Stack: ", errMsg);
         flash.system.System.exit(0);
     }
 	
