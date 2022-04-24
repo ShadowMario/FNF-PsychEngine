14c18749bda6a7df2f264e39f81864535500ac87
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-24 10:54:19 +0300

    Vibration in lua

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -41,6 +41,10 @@ import DialogueBoxPsych;
 import Discord;
 #end
 
+#if android
+import android.Hardware;
+#end
+
 using StringTools;
 
 class FunkinLua {
@@ -1442,6 +1446,11 @@ class FunkinLua {
 			#end
 		});
 
+		Lua_helper.add_callback(lua, "vibration", function(milliseconds:Int) {
+			#if android
+			Hardware.vibrate(milliseconds);
+			#end
+		});
 
 		// LUA TEXTS
 		Lua_helper.add_callback(lua, "makeLuaText", function(tag:String, text:String, width:Int, x:Float, y:Float) {
