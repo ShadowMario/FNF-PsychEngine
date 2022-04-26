1aa3f0f2236869bcc17278d3da510d75a2212512
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-26 07:59:33 +0300

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,6 +1,6 @@
 package;
 
-#if (android && MODS_ALLOWED)
+#if android
 import android.AndroidTools;
 import android.stuff.Permissions;
 #end
@@ -22,14 +22,14 @@ import flash.system.System;
 using StringTools;
 
 class SUtil {
-	#if (android && MODS_ALLOWED)
+	#if android
 	private static var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions(); 
 	private static var aDir:String = null; // android dir 
 	public static var sPath:String = AndroidTools.getExternalStorageDirectory(); // storage dir
 	#end
 
 	static public function getPath():String {
-		#if (android && MODS_ALLOWED)
+		#if android
 		if (aDir != null && aDir.length > 0) {
 			return aDir;
 		} else {
@@ -42,7 +42,7 @@ class SUtil {
 	}
 
 	static public function doTheCheck() {
-		#if (android && MODS_ALLOWED)
+		#if android
 		if (!grantedPermsList.contains(Permissions.READ_EXTERNAL_STORAGE) || !grantedPermsList.contains(Permissions.WRITE_EXTERNAL_STORAGE)) {
 			if (AndroidTools.sdkVersion > 23 || AndroidTools.sdkVersion == 23) {
 				AndroidTools.requestPermissions([Permissions.READ_EXTERNAL_STORAGE, Permissions.WRITE_EXTERNAL_STORAGE]);
@@ -114,13 +114,11 @@ class SUtil {
 
 		errMsg += e.error;
 
-		#if MODS_ALLOWED
 		if (!FileSystem.exists(SUtil.getPath() + "crash")){
 			FileSystem.createDirectory(SUtil.getPath() + "crash");
 		}
 
 		File.saveContent(SUtil.getPath() + path, errMsg + "\n");
-		#end
 
 		Sys.println(errMsg);
 		Sys.println("Crash dump saved in " + Path.normalize(path));
@@ -136,17 +134,17 @@ class SUtil {
 
 	#if android
 	static public function saveContent(fileName:String = "file", fileExtension:String = ".json", fileData:String = "you forgot something to add in your code"){
-		#if (android && MODS_ALLOWED)
 		if (!FileSystem.exists(SUtil.getPath() + "saves")){
 			FileSystem.createDirectory(SUtil.getPath() + "saves");
 		}
 
 		File.saveContent(SUtil.getPath() + "saves/" + fileName + fileExtension, fileData);
 		SUtil.applicationAlert("Done Action :)", "File Saved Successfully!");
-		#elseif android
+	}
+
+	static public function saveContent(fileName:String = "file", fileExtension:String = ".json", fileData:String = "you forgot something to add in your code"){
 		openfl.system.System.setClipboard(fileData);
 		SUtil.applicationAlert("Done Action :)", "Data Saved to Clipboard Successfully!");
-		#end
 	}
 
 	static public function copyContent(copyPath:String, savePath:String) {
@@ -155,5 +153,5 @@ class SUtil {
 			File.saveBytes(savePath, bytes);
 		}
 	}
-        #end
+	#end
 }
