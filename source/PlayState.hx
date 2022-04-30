3006d44e26f1ef34b8cf2bd26beadd0c9903b10b
nintendofan44 <85742902+nintendofan44@users.noreply.github.com>
2022-04-30 20:54:21 -0300

    nevermind, we dont need those 2

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3044,12 +3044,7 @@ class PlayState extends MusicBeatState
 				}
 			default: // maybe this will prevent crashes from non-existing events
 				var evtName:String = eventName;
-				var val1:Float = Std.parseFloat(value1);
-				var val2:Float = Std.parseFloat(value2);
-				if(Math.isNaN(val1)) val1 = 1;
-				if(Math.isNaN(val2)) val2 = 0;
-
-				trace('Such event does not exist. (Event Name: ' + evtName + ') | (Value 1:' + val1 + ') | (Value 2:' + val2 + ')');
+				trace('Such event does not exist. (Event Name: ' + evtName + ') | (Value 1:' + value1 + ') | (Value 2:' + value2 + ')');
 		}
 		callOnLuas('onEvent', [eventName, value1, value2]);
 	}
