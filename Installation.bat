264cd69c6a8d3c438c7a13b759cd1e741073098b
XS_09-Dev <81271362+XS09-dev@users.noreply.github.com>
2022-05-04 15:59:53 -0400

    Add files via upload

---
+++ Installation.bat
@@ -0,0 +1,17 @@
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib run lime setup flixel
+haxelib run lime setup
+haxelib install flixel-tools
+haxelib run flixel-tools setup
+haxelib update flixel
+haxelib install flixel
+haxelib install flixel-addons
+haxelib install flixel-ui
+haxelib install hscript
+haxelib install newgrounds
+haxelib git polymod https://github.com/larsiusprime/polymod.git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
+haxelib git linc_luajit https://github.com/AndreiRudenko/linc_luajit
