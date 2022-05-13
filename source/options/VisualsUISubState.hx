ed684793966e276dca7736a7147b62b0ae7e206e
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-05-13 20:16:58 +0300

    Update VisualsUISubState.hx

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -24,9 +24,6 @@ import flixel.util.FlxTimer;
 import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
 import Controls;
-#if android
-import android.Hardware;
-#end
 
 using StringTools;
 
