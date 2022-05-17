5c01a9c8cd4eb60abad4be96c3350cefbb8e799a
BeastlyGhost <45212377+BeastlyGhost@users.noreply.github.com>
2022-05-17 08:57:20 -0400

    there

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -574,7 +574,7 @@ class TitleState extends MusicBeatState
 				case 1:
 					FlxG.sound.music.stop();
 					FlxG.sound.playMusic(Paths.music('freakyMenu'), 0);
-					FlxG.sound.music.fadeIn(5, 0, 0.7);
+					FlxG.sound.music.fadeIn(4, 0, 0.7);
 				case 2:
 					#if PSYCH_WATERMARKS
 					createCoolText(['Psych Engine by'], 15);
