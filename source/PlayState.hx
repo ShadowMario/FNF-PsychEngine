795d7ddf54099d96b6e6a8f10b2daa894ef2c7e4
Intancote <sepandiusa@gmail.com>
2022-05-31 11:40:41 +0200

    Commits function sucks

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -70,7 +70,8 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
+		['How?!', 0]  //From 0%
+		['You Suck!', 0.2], //From 1% to 19%
 		['Shit', 0.4], //From 20% to 39%
 		['Bad', 0.5], //From 40% to 49%
 		['Bruh', 0.6], //From 50% to 59%
