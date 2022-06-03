8d9d25e8d705a0c4920480a28a35281f76d50b58
Ghost <45212377+BeastlyGhost@users.noreply.github.com>
2022-06-02 23:56:56 -0400

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -106,7 +106,12 @@ class OptionsState extends MusicBeatState
 
 		if (controls.BACK) {
 			FlxG.sound.play(Paths.sound('cancelMenu'));
-			MusicBeatState.switchState(new MainMenuState());
+			if (PauseSubState.toOptions) {
+				MusicBeatState.switchState(new PlayState());
+				FlxG.sound.music.stop();
+				} else {
+				MusicBeatState.switchState(new MainMenuState());
+				}
 		}
 
 		if (controls.ACCEPT) {
