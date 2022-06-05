3e15aa45dfd74d4cbd2b7a62f388e9ad377e9596
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-06-05 21:25:36 +0300

    Add files via upload

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -18,7 +18,7 @@ class Achievements {
 		["Lady Killer",					"Beat Week 4 on Hard with no Misses.",				'week4_nomiss',			false],
 		["Missless Christmas",			"Beat Week 5 on Hard with no Misses.",				'week5_nomiss',			false],
 		["Highscore!!",					"Beat Week 6 on Hard with no Misses.",				'week6_nomiss',			false],
-		["God Effing Damn It!",			"Beat Week 7 on Hard with no Misses.",				'week7_nomiss',			 true],
+		["God Effing Damn It!",			"Beat Week 7 on Hard with no Misses.",				'week7_nomiss',			false],
 		["What a Funkin' Disaster!",	"Complete a Song with a rating lower than 20%.",	'ur_bad',				false],
 		["Perfectionist",				"Complete a Song with a rating of 100%.",			'ur_good',				false],
 		["Roadkill Enthusiast",			"Watch the Henchmen die over 100 times.",			'roadkill_enthusiast',	false],
@@ -63,6 +63,17 @@ class Achievements {
 			}
 		}
 	}
+
+	public static function giveAchievement(achieve:String, achievementEnd:Void->Void = null):Void {
+		var achieveID:Int = Achievements.getAchievementIndex(achieve);
+		Main.toastManager.createToast(Paths.achievementImage('achievements/' + achieve), Achievements.achievementsStuff[achieveID][0], Achievements.achievementsStuff[achieveID][1], true);
+		if (achievementEnd != null)
+			Main.toastManager.onFinish = achievementEnd;
+
+		trace('Giving achievement ' + achieve);
+
+		ClientPrefs.saveSettings();
+	}
 }
 
 class AttachedAchievement extends FlxSprite {
@@ -95,65 +106,5 @@ class AttachedAchievement extends FlxSprite {
 			setPosition(sprTracker.x - 130, sprTracker.y + 25);
 
 		super.update(elapsed);
-	}
-}
-
-class AchievementObject extends FlxSpriteGroup {
-	public var onFinish:Void->Void = null;
-	var alphaTween:FlxTween;
-	public function new(name:String, ?camera:FlxCamera = null)
-	{
-		super(x, y);
-		ClientPrefs.saveSettings();
-
-		var id:Int = Achievements.getAchievementIndex(name);
-		var achievementBG:FlxSprite = new FlxSprite(60, 50).makeGraphic(420, 120, FlxColor.BLACK);
-		achievementBG.scrollFactor.set();
-
-		var achievementIcon:FlxSprite = new FlxSprite(achievementBG.x + 10, achievementBG.y + 10).loadGraphic(Paths.image('achievements/' + name));
-		achievementIcon.scrollFactor.set();
-		achievementIcon.setGraphicSize(Std.int(achievementIcon.width * (2 / 3)));
-		achievementIcon.updateHitbox();
-		achievementIcon.antialiasing = ClientPrefs.globalAntialiasing;
-
-		var achievementName:FlxText = new FlxText(achievementIcon.x + achievementIcon.width + 20, achievementIcon.y + 16, 280, Achievements.achievementsStuff[id][0], 16);
-		achievementName.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT);
-		achievementName.scrollFactor.set();
-
-		var achievementText:FlxText = new FlxText(achievementName.x, achievementName.y + 32, 280, Achievements.achievementsStuff[id][1], 16);
-		achievementText.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT);
-		achievementText.scrollFactor.set();
-
-		add(achievementBG);
-		add(achievementName);
-		add(achievementText);
-		add(achievementIcon);
-
-		var cam:Array<FlxCamera> = FlxCamera.defaultCameras;
-		if(camera != null) {
-			cam = [camera];
-		}
-		alpha = 0;
-		achievementBG.cameras = cam;
-		achievementName.cameras = cam;
-		achievementText.cameras = cam;
-		achievementIcon.cameras = cam;
-		alphaTween = FlxTween.tween(this, {alpha: 1}, 0.5, {onComplete: function (twn:FlxTween) {
-			alphaTween = FlxTween.tween(this, {alpha: 0}, 0.5, {
-				startDelay: 2.5,
-				onComplete: function(twn:FlxTween) {
-					alphaTween = null;
-					remove(this);
-					if(onFinish != null) onFinish();
-				}
-			});
-		}});
-	}
-
-	override function destroy() {
-		if(alphaTween != null) {
-			alphaTween.cancel();
-		}
-		super.destroy();
 	}
 }
