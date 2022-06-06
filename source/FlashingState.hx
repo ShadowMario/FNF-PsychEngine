cb0542f90142c9c563ac32cd4ec884b20ad12d8d
vidyagirl <67607376+vidyagirl@users.noreply.github.com>
2022-06-06 02:09:19 -0300

    add fade tween

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -47,8 +47,10 @@ class FlashingState extends MusicBeatState
 					ClientPrefs.flashing = false;
 					ClientPrefs.saveSettings();
 					FlxG.sound.play(Paths.sound('confirmMenu'));
-					new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-						MusicBeatState.switchState(new TitleState());
+					FlxTween.tween(warnText, {alpha: 0}, 1, {
+						onComplete: function (twn:FlxTween) {
+							MusicBeatState.switchState(new TitleState());
+						}
 					});
 					
 				} else {
