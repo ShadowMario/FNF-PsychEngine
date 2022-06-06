8ed01565c3885223f6786eafaa27e31a97922406
vidyagirl <67607376+vidyagirl@users.noreply.github.com>
2022-06-06 02:00:17 -0300

    Remove FlxFlicker from FlashingState
    It's pretty contradictory to flash lights kinda fast if a user doesn't want them

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -47,11 +47,10 @@ class FlashingState extends MusicBeatState
 					ClientPrefs.flashing = false;
 					ClientPrefs.saveSettings();
 					FlxG.sound.play(Paths.sound('confirmMenu'));
-					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
-						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
-							MusicBeatState.switchState(new TitleState());
-						});
+					new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+						MusicBeatState.switchState(new TitleState());
 					});
+					
 				} else {
 					FlxG.sound.play(Paths.sound('cancelMenu'));
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
