c13668e84fbf41abbb76feb6178a4cd3a30f355d
ImaginationSuperHero52806 <59747809+ImaginationSuperHero528@users.noreply.github.com>
2022-06-08 18:49:16 -0400

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -189,7 +189,7 @@ class FunkinLua {
 		set('noResetButton', ClientPrefs.noReset);
 		set('lowQuality', ClientPrefs.lowQuality);
 		set("scriptName", scriptName);
-		set('isPixelStage', PlayState.isPixelStage); // BTW idrk if these will actually work properly since I don't fully know what I'm doing with haxe lmao
+		set('isPixelStage', PlayState.isPixelStage); // BTW idrk if these will actually work properly since I don't fully know what I'm doing with haxe lmao I only have some understandings like how I had to add the "PlayState." part
 		set('isPixelStage', PlayState.curStage);
 
 		#if windows
