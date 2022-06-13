fc97c0846a546f053fa7e12ef919a6bb846b5401
8BitJake <75950907+8bitjake@users.noreply.github.com>
2022-06-13 11:58:02 -0500

    dammit how did i miss this

--- source/editors/CharacterEditorState.hx
+++ source/editors/CharacterEditorState.hx
@@ -922,11 +922,11 @@ class CharacterEditorState extends MusicBeatState
 			--i;
 		}
 		charLayer.clear();
-		ghostChar = new Character(0, 0, daAnim, !isDad);
+		ghostChar = new Character(0, 0, daAnim, !isDad, true);
 		ghostChar.debugMode = true;
 		ghostChar.alpha = 0.6;
 
-		char = new Character(0, 0, daAnim, !isDad);
+		char = new Character(0, 0, daAnim, !isDad, true);
 		if(char.animationsArray[0] != null) {
 			char.playAnim(char.animationsArray[0].anim, true);
 		}
