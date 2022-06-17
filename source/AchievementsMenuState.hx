8b15f8992992a1836e7121b924116fcc3ede9120
Ghost <45212377+BeastlyGhost@users.noreply.github.com>
2022-06-17 00:54:10 -0400

    Update AchievementsMenuState.hx

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -70,6 +70,13 @@ class AchievementsMenuState extends MusicBeatState
 		descText.scrollFactor.set();
 		descText.borderSize = 2.4;
 		add(descText);
+		
+		var resetText:FlxText = new FlxText(0, 680, FlxG.width, "Press R to reset achievement", 12);
+		resetText.borderSize = 5;
+		resetText.setFormat(Paths.font("vcr.ttf"), 28, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		resetText.scrollFactor.set();
+		add(resetText);
+
 		changeSelection();
 
 		super.create();
@@ -83,6 +90,11 @@ class AchievementsMenuState extends MusicBeatState
 		}
 		if (controls.UI_DOWN_P) {
 			changeSelection(1);
+		}
+		
+		if(controls.RESET) {
+			achievementArray[curSelected].forget();
+			grpOptions.members[curSelected].changeText('?');
 		}
 
 		if (controls.BACK) {
