02cdac057c90fdb7d1df6cef9925fa01cfd3069a
Ghost <45212377+BeastlyGhost@users.noreply.github.com>
2022-06-18 16:05:27 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1476,42 +1476,42 @@ class PlayState extends MusicBeatState
 	}
 
 	public function startVideo(name:String, loop:Bool = false, haccelerated:Bool = true, pauseMusic:Bool = false)
-    {
-        #if VIDEOS_ALLOWED
-        inCutscene = true;
+	{
+		#if VIDEOS_ALLOWED
+		inCutscene = true;
 
-        var filepath:String = Paths.video(name);
-        #if sys
-        if(!FileSystem.exists(filepath))
-        #else
-        if(!OpenFlAssets.exists(filepath))
-        #end
-        {
-            FlxG.log.warn('Couldnt find video file: ' + name);
-            startAndEnd();
-            return;
-        }
+		var filepath:String = Paths.video(name);
+		#if sys
+		if(!FileSystem.exists(filepath))
+		#else
+		if(!OpenFlAssets.exists(filepath))
+		#end
+		{
+		    FlxG.log.warn('Couldnt find video file: ' + name);
+		    startAndEnd();
+		    return;
+		}
 
-        var bg = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
-        bg.scrollFactor.set();
-        bg.cameras = [camHUD];
-        add(bg);
+		var bg = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
+		bg.scrollFactor.set();
+		bg.cameras = [camHUD];
+		add(bg);
 
-        var video:VideoHandler = new VideoHandler();
-        video.playVideo(filepath, loop, haccelerated, pauseMusic);
-        video.finishCallback = function()
-        {
-            remove(bg);
-            startAndEnd();
-            Paths.clearUnusedMemory();
-        }
-        return;
-        #else
-        FlxG.log.warn('Platform not supported!');
-        startAndEnd();
-        return;
-        #end
-    }
+		var video:VideoHandler = new VideoHandler();
+		video.playVideo(filepath, loop, haccelerated, pauseMusic);
+		video.finishCallback = function()
+		{
+		    remove(bg);
+		    startAndEnd();
+		    Paths.clearUnusedMemory();
+		}
+		return;
+		#else
+		FlxG.log.warn('Platform not supported!');
+		startAndEnd();
+		return;
+		#end
+    	}
 
 	function startAndEnd()
 	{
