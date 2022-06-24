1adb4bb82fd5fd36aead4915a688270af6ad00bb
jigsaw-4277821 <77043862+jigsaw-4277821@users.noreply.github.com>
2022-06-24 17:50:58 +0300

    Update

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -41,7 +41,7 @@ class SUtil
 		#end
 	}
 
-	public static function doTheCheck()
+	public static function doTheCheck():Void
 	{
 		#if android
 		if (!Permissions.getGrantedPermissions().contains(PermissionsList.READ_EXTERNAL_STORAGE) || !Permissions.getGrantedPermissions().contains(PermissionsList.WRITE_EXTERNAL_STORAGE))
@@ -52,49 +52,47 @@ class SUtil
 
 		if (Permissions.getGrantedPermissions().contains(PermissionsList.READ_EXTERNAL_STORAGE) || Permissions.getGrantedPermissions().contains(PermissionsList.WRITE_EXTERNAL_STORAGE))
 		{
-			if (!FileSystem.exists(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file')))
-				FileSystem.createDirectory(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file'));
+			if (!FileSystem.exists(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/'))
+				FileSystem.createDirectory(Tools.getExternalStorageDirectory() + '/' + '.' + Application.current.meta.get('file') + '/');
 
-			if (!FileSystem.exists(SUtil.getPath() + 'assets') && !FileSystem.exists(SUtil.getPath() + 'mods'))
+			if (!FileSystem.exists(SUtil.getPath() + 'assets/') && !FileSystem.exists(SUtil.getPath() + 'mods/'))
 			{
 				SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the files from the .APK!\nPlease watch the tutorial by pressing OK.");
-				CoolUtil.browserLoad('https://youtu.be/zjvkTmdWvfU');
-				System.exit(0);
+				openLinkAndClose();
 			}
 			else
 			{
-				if (!FileSystem.exists(SUtil.getPath() + 'assets'))
+				if (!FileSystem.exists(SUtil.getPath() + 'assets/'))
 				{
 					SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the assets/assets folder from the .APK!\nPlease watch the tutorial by pressing OK.");
-					CoolUtil.browserLoad('https://youtu.be/zjvkTmdWvfU');
-					System.exit(0);
+					openLinkAndClose();
 				}
-
-				if (!FileSystem.exists(SUtil.getPath() + 'mods'))
+				if (!FileSystem.exists(SUtil.getPath() + 'mods/'))
 				{
 					SUtil.applicationAlert('Uncaught Error :(!', "Whoops, seems you didn't extract the assets/mods folder from the .APK!\nPlease watch the tutorial by pressing OK.");
-					CoolUtil.browserLoad('https://youtu.be/zjvkTmdWvfU');
-					System.exit(0);
+					openLinkAndClose();
 				}
 			}
 		}
 		#end
 	}
 
-	public static function gameCrashCheck()
+	public static function gameCrashCheck():Void
 	{
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
 	}
 
-	public static function onCrash(e:UncaughtErrorEvent):Void
+	function onCrash(e:UncaughtErrorEvent):Void
 	{
+		var errMsg:String = "";
+		var path:String;
 		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
 		var dateNow:String = Date.now().toString();
-		dateNow = StringTools.replace(dateNow, " ", "_");
-		dateNow = StringTools.replace(dateNow, ":", "'");
 
-		var path:String = "crash/" + "crash_" + dateNow + ".txt";
-		var errMsg:String = "";
+		dateNow = dateNow.replace(" ", "_");
+		dateNow = dateNow.replace(":", "'");
+
+		path = "crash/" + Application.current.meta.get('file') + "_" + dateNow + ".txt";
 
 		for (stackItem in callStack)
 		{
@@ -107,43 +105,51 @@ class SUtil
 			}
 		}
 
-		errMsg += e.error;
+		errMsg += "\nUncaught Error: " + e.error;
 
-		if (!FileSystem.exists(SUtil.getPath() + "crash"))
-		FileSystem.createDirectory(SUtil.getPath() + "crash");
+		if (!FileSystem.exists(SUtil.getPath() + "crash/"))
+			FileSystem.createDirectory(SUtil.getPath() + "crash/");
 
 		File.saveContent(SUtil.getPath() + path, errMsg + "\n");
 
 		Sys.println(errMsg);
 		Sys.println("Crash dump saved in " + Path.normalize(path));
-		Sys.println("Making a simple alert ...");
 
-		SUtil.applicationAlert("Uncaught Error :(!", errMsg);
-		System.exit(0);
+		Application.current.window.alert(errMsg, "Error!");
+		#if desktop
+		DiscordClient.shutdown();
+		#end
+		Sys.exit(1);
 	}
 
-	private static function applicationAlert(title:String, description:String)
+	private static function applicationAlert(title:String, description:String):Void
 	{
 		Application.current.window.alert(description, title);
 	}
 
-	#if android
-	public static function saveContent(fileName:String = 'file', fileExtension:String = '.json', fileData:String = 'you forgot something to add in your code')
+	private static function openLinkAndClose():Void
 	{
-		if (!FileSystem.exists(SUtil.getPath() + 'saves'))
-			FileSystem.createDirectory(SUtil.getPath() + 'saves');
+		CoolUtil.browserLoad('https://youtu.be/zjvkTmdWvfU');
+		Sys.exit(1);
+	}
+
+	#if android
+	public static function saveContent(fileName:String = 'file', fileExtension:String = '.json', fileData:String = 'you forgot something to add in your code'):Void
+	{
+		if (!FileSystem.exists(SUtil.getPath() + 'saves/'))
+			FileSystem.createDirectory(SUtil.getPath() + 'saves/');
 
 		File.saveContent(SUtil.getPath() + 'saves/' + fileName + fileExtension, fileData);
-		SUtil.applicationAlert('Done :)!', 'File Saved Successfully!');
+		SUtil.applicationAlert('Done!', 'File Saved Successfully!');
 	}
 
-	public static function saveClipboard(fileData:String = 'you forgot something to add in your code')
+	public static function saveClipboard(fileData:String = 'you forgot something to add in your code'):Void
 	{
 		openfl.system.System.setClipboard(fileData);
-		SUtil.applicationAlert('Done :)!', 'Data Saved to Clipboard Successfully!');
+		SUtil.applicationAlert('Done!', 'Data Saved to Clipboard Successfully!');
 	}
 
-	public static function copyContent(copyPath:String, savePath:String)
+	public static function copyContent(copyPath:String, savePath:String):Void
 	{
 		if (!FileSystem.exists(savePath))
 			File.saveBytes(savePath, OpenFlAssets.getBytes(copyPath));
