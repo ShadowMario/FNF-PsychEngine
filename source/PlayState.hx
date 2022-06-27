aabe8b79509e1754573c3a66049152c8c3965f85
A Crazy Town <luigibros626@gmail.com>
2022-06-27 13:39:19 +0200

    optimize spritesheets

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -64,7 +64,7 @@ import sys.FileSystem;
 #end
 
 #if VIDEOS_ALLOWED
-import vlc.MP4Handler;
+import vlc.VideoHandler;
 #end
 
 using StringTools;
@@ -1493,7 +1493,7 @@ class PlayState extends MusicBeatState
 			return;
 		}
 
-		var video:MP4Handler = new MP4Handler();
+		var video:VideoHandler = new VideoHandler();
 		video.playVideo(filepath);
 		video.finishCallback = function()
 		{
