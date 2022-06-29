b6cc01ec09a7aaec19fe279bbc554e83586ad006
jigsaw-4277821 <77043862+jigsaw-4277821@users.noreply.github.com>
2022-06-29 15:46:21 +0300

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -84,10 +84,10 @@ class FunkinLua {
 			var resultStr:String = Lua.tostring(lua, result);
 			if(resultStr != null && result != 0) {
 				trace('Error on lua script! ' + resultStr);
-				#if windows
+				#if (windows || android)
 				lime.app.Application.current.window.alert(resultStr, 'Error on lua script!');
 				#else
-				luaTrace('Error loading lua script: ' + script.replace(SUtil.getPath(), "") + '\n' + resultStr, true, false, FlxColor.RED);
+				luaTrace('Error loading lua script: "$script"\n' + resultStr, true, false, FlxColor.RED);
 				#end
 				lua = null;
 				return;
