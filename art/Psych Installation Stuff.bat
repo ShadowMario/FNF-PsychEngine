d8202f45d87ed45da4a93a9712e7406747b846c2
ImaginationSuperHero52806 <59747809+ImaginationSuperHero528@users.noreply.github.com>
2022-07-01 23:00:34 -0400

    Create Psych Installation Stuff.bat
    Basically everything ready for first time use!

---
+++ art/Psych Installation Stuff.bat
@@ -0,0 +1,37 @@
+haxelib setup C:/haxelib
+
+haxelib install hxcpp > nul
+
+haxelib install lime
+
+haxelib install openfl
+
+haxelib install flixel
+
+haxelib run lime setup flixel
+
+haxelib run lime setup
+
+haxelib install flixel-tools
+
+haxelib install flixel-ui
+
+haxelib install flixel-addons
+
+haxelib install tjson
+
+haxelib install hxjsonast
+
+haxelib install hxCodec
+
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit
+
+haxelib install hscript
+
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
+
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+
+haxelib install hxcpp-debug-server
+
+haxelib list
