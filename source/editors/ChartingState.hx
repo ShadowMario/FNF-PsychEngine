b7521b990d1e0079fd83709b067a38e17ee8973d
Raltyro <78720179+Raltyro@users.noreply.github.com>
2022-07-03 22:00:45 +0700

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -2907,25 +2907,38 @@ class ChartingState extends MusicBeatState
 		//shitty null fix, i fucking hate it when this happens
 		//make it look sexier if possible
 		try {
+			var defaultDiff:String = CoolUtil.defaultDifficulty.toLowerCase();
+			var songLower:String = song.toLowerCase();
+			
 			var ind:Int = song.lastIndexOf("-");
 			var success:Bool = false;
 			if (ind != -1) {
 				try {
-					PlayState.SONG = Song.loadFromJson(song.toLowerCase(), song.substring(0, ind).toLowerCase());
+					PlayState.SONG = Song.loadFromJson(songLower, songLower.substring(0, ind));
 					success = true;
+					
+					// after loaded
+					var diff:String = songLower.substring(ind + 1);
+					var ind:Int = CoolUtil.lowerDifficulties.indexOf(diff);
+					if (ind != -1) PlayState.storyDifficulty = ind;
 				}
 				catch(e) {}
 			}
 			if (!success) {
-				if (CoolUtil.difficulties[PlayState.storyDifficulty] != CoolUtil.defaultDifficulty) {
-					if(CoolUtil.difficulties[PlayState.storyDifficulty] == null){
-						PlayState.SONG = Song.loadFromJson(song.toLowerCase(), song.toLowerCase());
-					}else{
-						PlayState.SONG = Song.loadFromJson(song.toLowerCase() + "-" + CoolUtil.difficulties[PlayState.storyDifficulty], song.toLowerCase());
+				var diff:String = CoolUtil.difficulties[PlayState.storyDifficulty];
+				if (diff != null) diff = diff.toLowerCase();
+				
+				if (diff != null && diff != defaultDiff) {
+					try {
+						PlayState.SONG = Song.loadFromJson(songLower + "-" + diff, songLower);
 					}
-				}else{
-					PlayState.SONG = Song.loadFromJson(song.toLowerCase(), song.toLowerCase());
+					catch(e) {}
 				}
+				PlayState.SONG = Song.loadFromJson(songLower, songLower);
+				
+				// after loaded
+				var ind:Int = CoolUtil.lowerDifficulties.indexOf(defaultDiff);
+				if (ind != -1) PlayState.storyDifficulty = ind;
 			}
 		}
 		catch(e) {
