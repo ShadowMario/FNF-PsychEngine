1515d0fe83974b3d4ab4a566050867fb47326878
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-07-14 18:31:55 -0300

    Update Song.hx

--- source/Song.hx
+++ source/Song.hx
@@ -100,7 +100,7 @@ class Song
 		#end
 
 		if(rawJson == null) {
-			#if sys
+			#if windows
 			rawJson = File.getContent(Paths.json(formattedFolder + '/' + formattedSong)).trim();
 			#else
 			rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();
