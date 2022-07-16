5a538c91389131837a31a92dd93824382be03f3c
Unholywanderer <96925902+Unholywanderer@users.noreply.github.com>
2022-07-16 15:30:16 -0700

    aaaaaaaaaaa

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -78,7 +78,7 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 		addOption(option);
 
 		var option:Option = new Option('Hitsound Volume',
-			'Funny notes does \"Tick!\" when you hit them."',
+			'Funny notes does \"Tick!\" when you hit them.',
 			'hitsoundVolume',
 			'percent',
 			0);
