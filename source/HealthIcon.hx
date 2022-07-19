d07742eef6791e7b4ef6abfad15a3a7eb41d3d21
Intancote <sepandiusa@gmail.com>
2022-07-19 19:47:05 +0200

    Winning icon support (no icons yet)

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -43,12 +43,13 @@ class HealthIcon extends FlxSprite
 			var file:Dynamic = Paths.image(name);
 
 			loadGraphic(file); //Load stupidly first for getting the file size
-			loadGraphic(file, true, Math.floor(width / 2), Math.floor(height)); //Then load it fr
-			iconOffsets[0] = (width - 150) / 2;
-			iconOffsets[1] = (width - 150) / 2;
+			loadGraphic(file, true, Math.floor(width / 3), Math.floor(height)); //Then load it fr
+			iconOffsets[0] = (width - 150) / 3;
+			iconOffsets[1] = (width - 150) / 3;
+			iconOffsets[2] = (width - 150) / 3;
 			updateHitbox();
 
-			animation.add(char, [0, 1], 0, false, isPlayer);
+			animation.add(char, [0, 1, 2], 0, false, isPlayer);
 			animation.play(char);
 			this.char = char;
 
