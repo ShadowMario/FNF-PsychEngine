5e4033915844e4c443ec80ef764d73c25ae04d4d
Intancote <sepandiusa@gmail.com>
2022-07-19 21:41:07 +0200

    Fixed the wincon support and added bf pix wincon

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2942,11 +2942,15 @@ class PlayState extends MusicBeatState
 
 		if (healthBar.percent < 20)
 			iconP1.animation.curAnim.curFrame = 1;
+		else if (healthBar.percent > 80)
+			iconP1.animation.curAnim.curFrame = 2;
 		else
 			iconP1.animation.curAnim.curFrame = 0;
 
 		if (healthBar.percent > 80)
 			iconP2.animation.curAnim.curFrame = 1;
+		else if (healthBar.percent < 20)
+			iconP2.animation.curAnim.curFrame = 2;
 		else
 			iconP2.animation.curAnim.curFrame = 0;
 
