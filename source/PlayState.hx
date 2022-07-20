dcfc4167c7a2ae0e23861230cd62c91e35fcd183
The Average GitHub User <106763399+EpokCoolGuy@users.noreply.github.com>
2022-07-20 17:34:10 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3547,6 +3547,7 @@ class PlayState extends MusicBeatState
 				}
 
 			case 'Screen Shake':
+		if(ClientPrefs.shaking) {
 				var valuesArray:Array<String> = [value1, value2];
 				var targetsArray:Array<FlxCamera> = [camGame, camHUD];
 				for (i in 0...targetsArray.length) {
@@ -3562,6 +3563,7 @@ class PlayState extends MusicBeatState
 						targetsArray[i].shake(intensity, duration);
 					}
 				}
+		}
 
 
 			case 'Change Character':
