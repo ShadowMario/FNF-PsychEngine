747bfbe314f62a1c75baf14d4d6c2014eecf394b
Mastercopper2346 <101440491+Mastercopper2346@users.noreply.github.com>
2022-07-21 16:08:52 -0700

    Nevermind

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -81,9 +81,6 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 		option.changeValue = 0.1;
 		option.displayFormat = '%vX';
 		optionsArray.push(option);
-		
-		var option:GameplayOption = new GameplayOption('Difficulty health function', 'bool', true);
-		optionsArray.push(option);
 
 		var option:GameplayOption = new GameplayOption('Instakill on Miss', 'instakill', 'bool', false);
 		optionsArray.push(option);
