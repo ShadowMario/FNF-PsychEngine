778c1e689d5be9d57bf6fa9bce6baf93c25e15b5
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-08-14 09:25:10 -0300

    Update DialogueBoxPsych.hx

--- source/DialogueBoxPsych.hx
+++ source/DialogueBoxPsych.hx
@@ -293,7 +293,19 @@ class DialogueBoxPsych extends FlxSpriteGroup
 			bgFade.alpha += 0.5 * elapsed;
 			if(bgFade.alpha > 0.5) bgFade.alpha = 0.5;
 
-			if(PlayerSettings.player1.controls.ACCEPT) {
+		        #if android
+                        var justTouched:Bool = false;
+
+		        for (touch in FlxG.touches.list)
+		        {
+			        if (touch.justPressed)
+			        {
+				        justTouched = true;
+			        }
+		        }
+		        #end
+
+			if(PlayerSettings.player1.controls.ACCEPT #if android || justTouched #end) {
 				if(!daText.finishedText) {
 					if(daText != null) {
 						daText.killTheTimer();
