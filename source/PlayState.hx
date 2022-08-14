3144a6fe0255779424e45aa2b90eba385d21edbc
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-08-14 12:27:54 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2002,11 +2002,9 @@ class PlayState extends MusicBeatState
 		var ret:Dynamic = callOnLuas('onStartCountdown', [], false);
 		if(ret != FunkinLua.Function_Stop) {
 			if (skipCountdown || startOnTime > 0) skipArrowStartTween = true;
-
 		        #if android
 		        androidControls.visible = true;
 		        #end
-
 			generateStaticArrows(0);
 			generateStaticArrows(1);
 			for (i in 0...playerStrums.length) {
