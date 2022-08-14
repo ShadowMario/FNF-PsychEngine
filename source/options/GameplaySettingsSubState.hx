51b35751933aaeb784dd661dd46ff0f454c17e5e
Amogus124 <onixorochinho@gmail.com>
2022-08-14 14:16:03 -0400

    algo de errado não está certo

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -38,7 +38,7 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 			'Check this if you want to play with\na controller instead of using your Keyboard.',
 			'controllerMode',
 			'bool',
-			#if android true #else false #end); //I play with controller, so 
+			true);
 		addOption(option);
 
 		//I'd suggest using "Downscroll" as an example for making your own option since it is the simplest here
