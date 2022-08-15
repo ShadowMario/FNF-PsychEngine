290dcdcbfbcae7318682e6887a3907c29b050bd1
Stefan{BETA}™(S.M) <109828559+StefanBETA2008@users.noreply.github.com>
2022-08-16 01:26:13 +0200

    Za Discord

---
+++ source/SBEngineDiscordStatus.hx
@@ -0,0 +1,106 @@
+package;
+
+#if sys
+import Sys.sleep;
+#end
+import discord_rpc.DiscordRpc;
+
+#if LUA_ALLOWED
+import llua.Lua;
+import llua.State;
+#end
+
+using StringTools;
+
+class DiscordClient
+{
+	public static var isInitialized:Bool = false;
+	public function new()
+	{
+		trace("Discord Klijent Zapocinje...");
+		DiscordRpc.start({
+			clientID: "1003474545107284088",
+			onReady: onReady,
+			onError: onError,
+			onDisconnected: onDisconnected
+		});
+		trace("Discord Klijent Zapocet.");
+
+		while (true)
+		{
+			DiscordRpc.process();
+			#if sys
+			sleep(2);
+			#end
+			//trace("Discord Client Update");
+		}
+
+		DiscordRpc.shutdown();
+	}
+	
+	public static function shutdown()
+	{
+		DiscordRpc.shutdown();
+	}
+	
+	static function onReady()
+	{
+		DiscordRpc.presence({
+			details: "Na Pocetnom Je",
+			state: null,
+			largeImageKey: 'icon',
+			largeImageText: "SB Engine"
+		});
+	}
+
+	static function onError(_code:Int, _message:String)
+	{
+		trace('Error! $_code : $_message');
+	}
+
+	static function onDisconnected(_code:Int, _message:String)
+	{
+		trace('Disconnected! $_code : $_message');
+	}
+
+	public static function initialize()
+	{
+		var DiscordDaemon = sys.thread.Thread.create(() ->
+		{
+			new DiscordClient();
+		});
+		trace("Discord Client initialized");
+		isInitialized = true;
+	}
+
+	public static function changePresence(details:String, state:Null<String>, ?smallImageKey : String, ?hasStartTimestamp : Bool, ?endTimestamp: Float)
+	{
+		var startTimestamp:Float = if(hasStartTimestamp) Date.now().getTime() else 0;
+
+		if (endTimestamp > 0)
+		{
+			endTimestamp = startTimestamp + endTimestamp;
+		}
+
+		DiscordRpc.presence({
+			details: details,
+			state: state,
+			largeImageKey: 'icon',
+			largeImageText: "Engine Verzija: " + MainMenuState.psychEngineVersion,
+			smallImageKey : smallImageKey,
+			// Obtained times are in milliseconds so they are divided so Discord can use it
+			startTimestamp : Std.int(startTimestamp / 1000),
+			endTimestamp : Std.int(endTimestamp / 1000)
+		});
+
+		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
+	}
+
+	#if LUA_ALLOWED
+	public static function addLuaCallbacks(lua:State) {
+		Lua_helper.add_callback(lua, "changePresence", function(details:String, state:Null<String>, ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float) {
+			changePresence(details, state, smallImageKey, hasStartTimestamp, endTimestamp);
+		});
+	}
+	#end
+}
