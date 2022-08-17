5ad3b06d24991440c7a234ac29e4fb1d8c68984a
Stefan{BETA}™(S.M) <109828559+StefanBETA2008@users.noreply.github.com>
2022-08-17 04:15:56 +0200

    Icon Bounce?

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1782,6 +1782,29 @@ class PlayState extends MusicBeatState
 			gf.dance();
 		};
 
+		//health icon bounce but epic
+		if (curBeat % gfSpeed == 0) {
+		curBeat % (gfSpeed * 2) == 0 ? {
+		iconP1.scale.set(1.1, 0.8);
+		iconP2.scale.set(1.1, 1.3);
+
+		FlxTween.angle(iconP1, -15, 0, Conductor.crochet / 1300, {ease: FlxEase.quadOut});
+		FlxTween.angle(iconP2, 15, 0, Conductor.crochet / 1300, {ease: FlxEase.quadOut});
+		} : {
+		iconP1.scale.set(1.1, 1.3);
+		iconP2.scale.set(1.1, 0.8);
+
+		FlxTween.angle(iconP2, -15, 0, Conductor.crochet / 1300, {ease: FlxEase.quadOut});
+		FlxTween.angle(iconP1, 15, 0, Conductor.crochet / 1300, {ease: FlxEase.quadOut});
+		}
+
+		FlxTween.tween(iconP1, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
+		FlxTween.tween(iconP2, {'scale.x': 1, 'scale.y': 1}, Conductor.crochet / 1250 * gfSpeed, {ease: FlxEase.quadOut});
+
+		iconP1.updateHitbox();
+		iconP2.updateHitbox();
+		}
+
 		camFollow.set(dad.x + 280, dad.y + 170);
 		switch(songName)
 		{
