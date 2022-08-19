609e50ed863e70eb478ff4638fc1dad9d9fabb25
Raltyro <78720179+Raltyro@users.noreply.github.com>
2022-08-19 08:12:15 +0700

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5079,7 +5079,7 @@ class PlayState extends MusicBeatState
 			
 			// had to do this because there is a bug in haxe where Stop != Continue doesnt work
 			var bool:Bool = ret == FunkinLua.Function_Continue;
-			if(!bool) {
+			if(!bool && ret != 0) {
 				returnVal = cast ret;
 			}
 		}
