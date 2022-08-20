46283cddbfcb1b993d7893e78295f959084a6df3
Stefan{BETA}™(S.M) <109828559+StefanBETA2008@users.noreply.github.com>
2022-08-20 02:02:49 +0200

    Ništa Od Toga

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -4,8 +4,6 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.text.FlxText;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.effects.FlxFlicker;
 import lime.app.Application;
@@ -21,15 +19,6 @@ class OutdatedState extends MusicBeatState
 	override function create()
 	{
 		super.create();
-
-		var sbLogo:FlxSprite = new FlxSprite(FlxG.width, 0).loadGraphic(Paths.image('SBEngineEngineLogo'));
-		sbLogo.scale.y = 0.3;
-		sbLogo.scale.x = 0.3;
-		sbLogo.x -= kadeLogo.frameHeight;
-		sbLogo.y -= 180;
-		sbLogo.alpha = 0.8;
-		sbLogo.antialiasing = FlxG.save.data.antialiasing;
-		add(kadeLogo);
 
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
@@ -58,35 +47,9 @@ class OutdatedState extends MusicBeatState
 		add(warnText);
 
 		#if android
-		addVirtualPad(NONE, A);
+		addVirtualPad(NONE, A_B);
 		#end
-
-		FlxTween.angle(sbLogo, sbLogo.angle, -10, 2, (ease: FlxEase);
-
-		new FlxTimer().start(2, function(tmr:FlxTimer);
-		{
-			FlxTween.color(bg, 2, bg.color, FlxColor.fromString(bgColors[colorRotation]));
-			if (colorRotation < (bgColors.length - 1))
-				colorRotation++;
-			else
-				colorRotation = 0;
-		}, 0);
-
-		new FlxTimer().start(2, function(tmr:FlxTimer)
-		{
-			if (sbLogo.angle == -10)
-				FlxTween.angle(sbLogo, sbLogo.angle, 10, 2, {ease: FlxEase});
-			else
-				FlxTween.angle(sbLogo, sbLogo.angle, -10, 2, {ease: FlxEase});
-		}, 0);
-
-		new FlxTimer().start(0.8, function(tmr:FlxTimer)
-		{
-			if (sbLogo.alpha == 0.8)
-				FlxTween.tween(sbLogo, {alpha: 1}, 0.8, {ease: FlxEase.});
-			else
-				FlxTween.tween(sbLogo, {alpha: 0.8}, 0.8, {ease: FlxEase});
-		}, 0);
+	}
 
 	override function update(elapsed:Float)
 	{
