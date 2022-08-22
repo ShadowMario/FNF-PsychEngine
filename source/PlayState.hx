29574712881ceb1d9da9cc91eb7800a590768445
91ebagacm <59393483+91ebagacm@users.noreply.github.com>
2022-08-22 22:59:22 +0300

    forget

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2687,7 +2687,7 @@ class PlayState extends MusicBeatState
 
 		super.update(elapsed);
 
-		scoreTxt.text = 'Score: ' + songScore + ' | Port by newbion | Misses: ' + songMisses + ' | Rating: ' + ratingName;
+		scoreTxt.text = 'Score: ' + songScore + ' | Misses: ' + songMisses + ' | Rating: ' + ratingName;
 		if(ratingName != '?')
 			scoreTxt.text += ' (' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%)' + ' - ' + ratingFC;
 
