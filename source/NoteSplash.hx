18d0e5b2d727b0de21b7c1b9d1aa84e52d1010b3
MaybeMaru <97055307+MaybeMaru@users.noreply.github.com>
2022-09-24 18:22:24 +0200

    Smaller code

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -50,9 +50,9 @@ class NoteSplash extends FlxSprite
 	function loadAnims(skin:String) {
 		frames = Paths.getSparrowAtlas(skin);
 		for (i in 1...3) {
+			animation.addByPrefix("note0-" + i, "note splash purple " + i, 24, false);
 			animation.addByPrefix("note1-" + i, "note splash blue " + i, 24, false);
 			animation.addByPrefix("note2-" + i, "note splash green " + i, 24, false);
-			animation.addByPrefix("note0-" + i, "note splash purple " + i, 24, false);
 			animation.addByPrefix("note3-" + i, "note splash red " + i, 24, false);
 		}
 	}
