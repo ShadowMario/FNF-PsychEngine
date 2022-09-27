80ba060192973e11cf1097733f9d6f8ca53e9b89
The Average GitHub User <106763399+TheAverageGitHubUser@users.noreply.github.com>
2022-09-27 19:15:55 -0300

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -11,7 +11,7 @@ class ClientPrefs {
 	public static var middleScroll:Bool = false;
 	public static var opponentStrums:Bool = true;
 	public static var showFPS:Bool = true;
-        public static var shaking:Bool = true
+        public static var shaking:Bool = true;
 	public static var flashing:Bool = true;
 	public static var globalAntialiasing:Bool = true;
 	public static var noteSplashes:Bool = true;
