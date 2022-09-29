f63ceccf26a3832cca355a44a99d5fe51e3661ec
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-09-29 17:14:45 -0400

    Thing to show sections said beats

--- source/editors/EditorPlayState.hx
+++ source/editors/EditorPlayState.hx
@@ -501,7 +501,7 @@ class EditorPlayState extends MusicBeatState
 
 		keyShit();
 		scoreTxt.text = 'Hits: ' + songHits + ' | Misses: ' + songMisses;
-		sectionTxt.text = 'Beat: ' + curSection;
+		sectionTxt.text = 'Section: ' + curSection;
 		beatTxt.text = 'Beat: ' + curBeat;
 		stepTxt.text = 'Step: ' + curStep;
 		super.update(elapsed);
