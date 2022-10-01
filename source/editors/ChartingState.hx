d89634dc724cc046c96af9d9456e071645549bfe
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-10-01 15:01:52 -0400

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -1756,10 +1756,9 @@ class ChartingState extends MusicBeatState
 
 
 			if (FlxG.keys.justPressed.BACKSPACE) {
-				//if(onMasterEditor) {
-					MusicBeatState.switchState(new editors.MasterEditorMenu());
-					FlxG.sound.playMusic(Paths.music('freakyMenu'));
-				//}
+				PlayState.instance.chartingMode = false;
+				MusicBeatState.switchState(new editors.MasterEditorMenu());
+				FlxG.sound.playMusic(Paths.music('freakyMenu'));
 				FlxG.mouse.visible = false;
 				return;
 			}
