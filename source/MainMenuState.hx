3697263a2d182bf4fffd9c234485d922dda14781
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-10-02 17:14:48 -0400

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -130,7 +130,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "AumSum Funkin'", 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "AumSum Funkin' Pre-release", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
