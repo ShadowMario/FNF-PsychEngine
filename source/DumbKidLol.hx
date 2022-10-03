43bc618225846679d61b9b348ca73f9e5814e936
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-10-03 18:33:38 -0400

    Create DumbKidLol.hx

---
+++ source/DumbKidLol.hx
@@ -0,0 +1,28 @@
+package;
+
+class DumbKidLol extends RealDumbass
+{
+    var bitches:Int = 1;
+    var family:Bool = true;
+    var gotalife:Bool = true;
+    var adopted:Bool = false;
+    var STUPID:Bool = false;
+    var ratio:Int = 0;
+    var hasBrain:Bool = true;
+    var stupidKid:String = 'NimuDevUwU';
+
+    override function create()
+    {
+        if (stupidKid == 'NimuDevUwU') 
+        {
+            bitches = 0;
+            family = false;
+            gotalife = false;
+            adopted = true;
+            STUPID = true;
+            ratio++;
+            hasBrain = false;
+        }
+    }
+}
+//you know i had to do it to em
