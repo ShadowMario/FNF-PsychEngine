e47beb39d710f3c76ef820c1f40c82a3d6228e26
BeepSheep254 <113206839+BeepSheep254@users.noreply.github.com>
2022-10-03 11:06:53 -0400

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -91,7 +91,7 @@ class FPS extends TextField
 
 			if (memoryMegas > memoryMegasPeak) memoryMegasPeak = memoryMegas;
 
-			if (ClientPrefs.showMEM) text += "\nMEM: " + memoryMegas + " RAM" + "\nMEM PEAK: " + memoryMegasPeak + " RAM";
+			text += "\nMEM: " + memoryMegas + " RAM" + "\nMEM PEAK: " + memoryMegasPeak + " RAM";
 			#end
 
 			textColor = 0xFF9C9C9C;
