e5f936b1c8e50410ab0802297746e40cdc65df01
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-10-14 06:14:39 -0400

    make this a final

--- source/Character.hx
+++ source/Character.hx
@@ -78,7 +78,8 @@ class Character extends FlxSprite
 	public var originalFlipX:Bool = false;
 	public var healthColorArray:Array<Int> = [255, 0, 0];
 
-	public static var DEFAULT_CHARACTER:String = 'bf'; //In case a character is missing, it will use BF on its place
+	public static final DEFAULT_CHARACTER:String = 'bf'; //In case a character is missing, it will use BF on its place
+
 	public function new(x:Float, y:Float, ?character:String = 'bf', ?isPlayer:Bool = false)
 	{
 		super(x, y);
