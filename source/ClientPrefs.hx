5180e57f7da07b6d3fad4d8cd7ae521d52a0f5bb
Amogus124 <onixorochinho@gmail.com>
2022-11-02 02:56:33 -0400

    good

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -28,7 +28,7 @@ class ClientPrefs {
 	public static var scoreZoom:Bool = true;
 	public static var noReset:Bool = false;
 	public static var healthBarAlpha:Float = 1;
-	public static var controllerMode:Bool = false;
+	public static var controllerMode:Bool = true;
 	public static var hitsoundVolume:Float = 0;
 	public static var pauseMusic:String = 'Tea Time';
 	public static var checkForUpdates:Bool = true;
