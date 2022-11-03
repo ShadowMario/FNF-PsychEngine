4e4a58b6c1525990fc0c4c89fa239d96264c6209
ImaginationSuperHero52806 <okmilkrb@gmail.com>
2022-11-03 19:06:53 -0400

    Still working on :ShadowTroll:

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -917,11 +917,17 @@ class FunkinLua {
 			#end
 		});
 
-		Lua_helper.add_callback(lua, "loadSong", function(?name:String = null, ?difficultyNum:Int = -1) {
+		Lua_helper.add_callback(lua, "loadSong", function(?name:String = null, ?difficultyNum:Int = -1, ?weekJson:String = null) {
 			if(name == null || name.length < 1)
 				name = PlayState.SONG.song;
 			if (difficultyNum == -1)
 				difficultyNum = PlayState.storyDifficulty;
+			// MEGA FIX DIFFS BETWEEN WEEKS WILL NOW FUCKING WORK WOOOOOOOOOOOOW
+			if (weekJson == null || weekJson.length < 1) {
+				weekJson = WeekData.weeksList[PlayState.storyWeek];
+			} else {
+				weekJson = PlayState.storyDifficulty;
+			}
 
 			var poop = Highscore.formatSong(name, difficultyNum);
 			PlayState.SONG = Song.loadFromJson(poop, name);
