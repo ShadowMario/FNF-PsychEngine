79ff2df38fd96ed28144da7f2fbd1db81b2da76a
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-11-19 20:58:53 -0500

    stupidity
    Co-authored-by: Luisinho01 <91190561+Luisinhi010@users.noreply.github.com>

--- source/Main.hx
+++ source/Main.hx
@@ -114,6 +114,7 @@ class Main extends Sprite
 	function onCrash(e:UncaughtErrorEvent):Void
 	{
 		var errMsg:String = "";
+		var errMsgPrint:String = "";
 		var path:String;
 		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
 		var dateNow:String = Date.now().toString();
@@ -129,6 +130,7 @@ class Main extends Sprite
 			{
 				case FilePos(s, file, line, column):
 					errMsg += file + " (line " + line + ")\n";
+					errMsgPrint += file + ":" + line + "\n"; // if you Ctrl+Mouse Click its go to the line.
 				default:
 					Sys.println(stackItem);
 			}
@@ -141,7 +143,7 @@ class Main extends Sprite
 
 		File.saveContent(path, errMsg + "\n");
 
-		Sys.println(errMsg);
+		Sys.println(errMsgPrint);
 		Sys.println("Crash dump saved in " + Path.normalize(path));
 
 		Application.current.window.alert(errMsg, "Error!");
