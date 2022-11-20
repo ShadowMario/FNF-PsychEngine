3f8ea9b2e52383ce9b3449f42259ed70dfa31609
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-11-20 13:01:25 -0500

    QUOTES OMG

--- example_mods/scripts/info text.lua
+++ example_mods/scripts/info text.lua
@@ -1,19 +1,33 @@
-currentDifficulty = 'its still fucked';
+curentVersion = 0;
+
+local Quotes = {
+    "It's AumSum Time!",
+    "What if HScript Disappared?",
+    "No AumSum?",
+    "Thanks to Heli Pro Gamer for the fix!",
+    "No OpenFL?",
+    "MOM GET THE CAMERA",
+    "YouTube is gonna copyright strike you if you die!",
+    "oh noes arnold got his finger cut",
+    "what a loser, getting his mod cancelled",
+    "This is YOUR Daily Does of Internet",
+    "I've over dosed on ketamine and I'm going to die",
+    "Oh, AumSum.",
+    "hell nah aumsum have merch (its in www.aumsum.com)"
+}
 
 function onCreate()
-    makeLuaText('songText', songName .. " " .. currentDifficulty .. " - YouTube Engine 0.6.3", 0, 2, 701);
+   bit = string.gsub(version,"%.","")
+
+   curentVersion = tonumber(bit)
+end
+
+
+function onCreatePost()
+    makeLuaText('songText', songName .. ' - ' .. getProperty('storyDifficultyText') .. ' | YouTube Engine (PE 0.6.3) | ' .. Quotes[getRandomInt(1, 11)], 0, 2, 701);
     setTextAlignment('songText', 'left');
     setTextSize('songText', 15);
     setTextBorder('songText', 1, '000000');
+    setObjectCamera('songText', 'camHUD');
     addLuaText('songText');
-end
-
-function onCreatePost()
-    setProperty('timeBar.y', getProperty('timeBar.y') - 10);
-    setProperty('timeTxt.y', getProperty('timeTxt.y') - 10);
-end
-
-function onUpdate(elapsed)
-    currentDifficulty = getProperty('storyDifficultyText');
-    setTextString('songText', songName .. " " .. currentDifficulty .. " - Youtube Engine 0.6.3");
 end
