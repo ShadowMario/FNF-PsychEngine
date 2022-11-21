75da78ce4f458c55c7f79d2b8e8069733957e39b
Isaiah Mods (Official) <118643764+IsaiahModsOfficial@users.noreply.github.com>
2022-11-21 12:09:49 -0800

    joey stage files

---
+++ example_mods/stages/crazy-joey-live-show-stage.lua
@@ -0,0 +1,19 @@
+function onCreate()
+
+	makeLuaSprite('live show stage','joey live show stage',-750,-185)
+	addLuaSprite('live show stage',false)
+	close(true)
+
+end
+
+function onBeatHit()
+
+end
+
+function onStepHit()
+
+end
+
+function onUpdate()
+
+end
