e3fff66d6d85eeef3e8fa15b5ccb14d1d151a32e
Shadow Milky The Blob <62410083+ShadowMilky@users.noreply.github.com>
2022-12-08 21:09:57 -0500

    hehehehehehehehehehehe

---
+++ source/THISISSUPERUSEFUL.hx
@@ -0,0 +1,19 @@
+package; //your mom
+
+var slaying:Bool = true;
+var father:Bool = true;
+var loved:Bool = true;
+var btsFan:Bool = false;
+var pickme:Bool = false;
+var meena:Bool = false;
+
+class THISISSUPERUSEFUL {/* extends yourMom */
+  if(father){
+    slaying = true;
+    father = true;
+    loved = true;
+    btsFan = false;
+    pickme = false;
+    meena = false;
+  }
+}
