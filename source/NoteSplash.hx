d826ae68f51e53a88085f881431591565fca2a51
ImaginationSuperHero52806 <okmilkrb@gmail.com>
2023-02-04 19:17:38 -0500

    UGH

--- source/NoteSplash.hx
+++ source/NoteSplash.hx
@@ -14,7 +14,7 @@ class NoteSplash extends FlxSprite
 		super(x, y);
 
 		var skin:String = 'noteSplashes';
-		if((PlayState.SONG.splashSkin != null) && PlayState.SONG.splashSkin.length > 0) skin = PlayState.SONG.splashSkin;
+		if(PlayState.SONG.splashSkin != null && PlayState.SONG.splashSkin.length > 0) skin = PlayState.SONG.splashSkin;
 
 		loadAnims(skin);
 		
@@ -31,7 +31,7 @@ class NoteSplash extends FlxSprite
 		
 		if(texture == null || texture.length < 1) {
 			texture = 'noteSplashes';
-			if((PlayState.SONG.splashSkin != null) && PlayState.SONG.splashSkin.length > 0) texture = PlayState.SONG.splashSkin;
+			if(PlayState.SONG.splashSkin != null && PlayState.SONG.splashSkin.length > 0) texture = PlayState.SONG.splashSkin;
 		}
 
 		if(textureLoaded != texture) {
