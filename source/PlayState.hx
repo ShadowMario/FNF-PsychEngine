6f95895b2c2e781d2ef714d7d04f415daa30b882
Rodney, An Imaginative Person <59747809+RodneyAnImaginativePerson@users.noreply.github.com>
2023-02-07 00:31:44 -0500

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3798,10 +3798,10 @@ class PlayState extends MusicBeatState
 				
 				if (value2.length < 1) value2 = 'true';
 				if (value2 == 'true' && ClientPrefs.middleScroll) {
-					var alphaVal = [ClientPrefs.middleScroll];
+					var alphaVal = [ClientPrefs.middleScroll ? ];
 					for (i in 0...opponentStrums.members.length) FlxTween.tween(opponentStrums.members[i], {x: defaultPlayerStrum.x[i], alpha: alphaVal[0]}, 0.35, {ease: FlxEase.circOut});
 					for (i in 0...playerStrums.members.length) FlxTween.tween(playerStrums.members[i], {x: defaultOpponentStrum.x[i], alpha: alphaVal[1]}, 0.35, {ease: FlxEase.circOut});
-				} // else if (value2 == 'false') {trace('FUCK YOU NOTHING HAPPENED');}
+				} // else if (value2 == 'false') {trace('FUCK YOU NOTHING HAPPENED LMFAO');}
 
 				// So strums don't look wierd after switch.
 				for (i in 0...strumLineNotes.members.length) strumLineNotes.members[i].playAnim('static', true);
