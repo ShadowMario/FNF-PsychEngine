f7afd5c9b4a2c567d7c4c6ca26a8e859e48d799d
Rodney, An Imaginative Person <59747809+RodneyAnImaginativePerson@users.noreply.github.com>
2023-02-07 01:24:06 -0500

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4422,9 +4422,11 @@ class PlayState extends MusicBeatState
 	private function getKeyFromEvent(key:FlxKey):Int
 	{
 		if (key != NONE) {
-			for (i in 0...keysArray.length)
-				for (j in 0...keysArray[i].length)
+			for (i in 0...keysArray.length) {
+				for (j in 0...keysArray[i].length) {
 					if (key == keysArray[i][j]) return i;
+				}
+			}
 		}
 		return -1;
 	}
