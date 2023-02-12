fc56c597924bc6470d89f6c9f9490b0f04040c1f
Golden Tiux <golden.tiux@gmail.com>
2023-02-12 18:38:23 +0100

    Fix for 2.6.0 too
    Wow, the various versions really did move a lot of files LOL

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -73,6 +73,7 @@ import sys.io.File;
 
 #if VIDEOS_ALLOWED
 #if (hxCodec >= "2.6.1") import hxcodec.VideoHandler as MP4Handler;
+#elseif (hxCodec == "2.6.0") import VideoHandler as MP4Handler;
 #else import vlc.MP4Handler; #end
 #end
 
