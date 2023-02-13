667c7272c7149fba2509cc87996368e3c21ad621
Gabriela Bestial <93128334+BeastlyGabi@users.noreply.github.com>
2023-02-13 15:00:02 -0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -72,7 +72,7 @@ import sys.io.File;
 #end
 
 #if VIDEOS_ALLOWED
-#if (hxCodec >= "2.6.1") import hxcodec.VideoHandler
+#if (hxCodec >= "2.6.1") import hxcodec.VideoHandler;
 #elseif (hxCodec >= "2.6.0") import VideoHandler;
 #else import vlc.MP4Handler as VideoHandler; #end
 #end
