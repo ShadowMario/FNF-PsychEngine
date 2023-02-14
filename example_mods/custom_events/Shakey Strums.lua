0457ed91c893df20864e13ab6f4f222c363fa430
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-02-14 15:34:03 -0500

    fixed it

--- example_mods/custom_events/Shakey Strums.lua
+++ example_mods/custom_events/Shakey Strums.lua
@@ -2,18 +2,21 @@ local shakeAmount = 0
 
 local defaultNotePos = {}
 
-function onSongStart()
-	for i = 0,7 do 
-        x = getPropertyFromGroup('strumLineNotes', i, 'x')
-        y = getPropertyFromGroup('strumLineNotes', i, 'y')
+function onCreatePost()
+	for i = 0,getProperty('opponentStrums.length') do 
+        x = getPropertyFromGroup('opponentStrums', i, 'x')
+        y = getPropertyFromGroup('opponentStrums', i, 'y')
         table.insert(defaultNotePos, {x,y})
 		--remember first in array is 1: x = 1, y = 2
     end
 end
 
-for i=4,7 do
-	setPropertyFromGroup('strumLineNotes', i, 'x', defaultNotePos[i + 1][1] + math.random(-shakeAmount, shakeAmount))
-	setPropertyFromGroup('strumLineNotes', i, 'y', defaultNotePos[i + 1][2] + math.random(-shakeAmount, shakeAmount))
+function onUpdatePost()
+
+	for i=0,getProperty('opponentStrums.length') do
+		setPropertyFromGroup('opponentStrums', i, 'x', defaultNotePos[i + 1][1] + math.random(-shakeAmount, shakeAmount))
+		setPropertyFromGroup('opponentStrums', i, 'y', defaultNotePos[i + 1][2] + math.random(-shakeAmount, shakeAmount))
+	end	
 end
 
 function onEvent(name, value1, value2)
