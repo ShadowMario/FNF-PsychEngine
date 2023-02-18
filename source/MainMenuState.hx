5a7c7c4cccc7be1e8b2dfdcb6d19ab661bf685f7
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-02-17 21:49:05 -0500

    :skull:

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -56,7 +56,7 @@ class MainMenuState extends MusicBeatState
 
 		#if desktop
 		// Updating Discord Rich Presence
-		DiscordClient.changePresence("In the Menus", null);
+		DiscordClient.changePresence("Main Menu", null);
 		#end
 		debugKeys = ClientPrefs.copyKey(ClientPrefs.keyBinds.get('debug_1'));
 
