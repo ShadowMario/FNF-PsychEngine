2002d33a326bc8720f24cebbe2348487f57bfd6c
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-02-19 15:48:44 -0500

    tuskeegee

---
+++ assets/preload/stages/BlandTown.lua
@@ -0,0 +1,15 @@
+
+--How makeLuaSprite works:
+--makeLuaSprite(<SPRITE VARIABLE>, <SPRITE IMAGE FILE NAME>, <X>, <Y>);
+--"Sprite Variable" is how you refer to the sprite you just spawned in other methods like "setScrollFactor" and "scaleObject" for example
+
+--so for example, i made the sprites "stagelight_left" and "stagelight_right", i can use "scaleObject('stagelight_left', 1.1, 1.1)"
+--to adjust the scale of specifically the one stage light on left instead of both of them
+
+function onCreate()
+	-- background shit
+	makeLuaSprite('BlandTown', 'BlandTown', 0, 0);
+	setScrollFactor('BlandTown', 0.3, 0.3);
+
+	addLuaSprite('BlandTown', false);
+end
