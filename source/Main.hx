8154da4d43cdd862c3c793c695057453c7a11e04
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-02-24 18:32:34 -0500

    trying to fix a error

--- source/Main.hx
+++ source/Main.hx
@@ -26,6 +26,10 @@ import sys.io.File;
 import sys.io.Process;
 #end
 
+#if (hxCodec > "2.5.1")
+#error "hxCodec is greater than 2.5.1, please use 2.5.1 instead"
+#end
+
 using StringTools;
 
 class Main extends Sprite
