8a19b633a33a613f4ff4c38c9ffa63b84a3f7730
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-02-26 11:48:49 -0500

    please work PLEASE

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -91,7 +91,11 @@ class ChartingState extends MusicBeatState
 		['Screen Shake', "Value 1: Camera shake\nValue 2: HUD shake\n\nEvery value works as the following example: \"1, 0.05\".\nThe first number (1) is the duration.\nThe second number (0.05) is the intensity."],
 		['Change Character', "Value 1: Character to change (Dad, BF, GF)\nValue 2: New character's name"],
 		['Change Scroll Speed', "Value 1: Scroll Speed Multiplier (1 is default)\nValue 2: Time it takes to change fully in seconds."],
-		['Set Property', "Value 1: Variable name\nValue 2: New value"]
+		['Set Property', "Value 1: Variable name\nValue 2: New value"],
+		['CinematicBars', "Black shitty lines on the screen\nValue 1: 1 is on, 2 is off\nValue 2: Speed"],
+		['Flash Camera', "Flashes the screen\nValue 1: speed"],//not defining value 2, you just basically leave it blank
+		['OpponentShakeyStrums', "Shakes the opponents notes\nValue 1: Amount\nNote: you can't stop the strums once their triggered"],//value 2, AGAIN is just blank
+		['Bubbles Week Events', "Background characters, its simple"]//not correctly coded in yet...
 	];
 
 	var _file:FileReference;
