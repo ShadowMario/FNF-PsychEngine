b7066b226655ccbb930d26740a666ce8ea0f2b4c
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-02-27 17:52:53 -0500

    so i stole some fucking code from echo engine
    so funny i even made echo engine 🤯

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,6 +26,7 @@ using StringTools;
 class MainMenuState extends MusicBeatState
 {
 	public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
+	public static var vsStupidVersion:String = '1 ( DEMO BUILD )'; // dunno why i stole this from echo engine...
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
@@ -126,7 +127,13 @@ class MainMenuState extends MusicBeatState
 		}
 
 		FlxG.camera.follow(camFollowPos, null, 1);
-
+                
+		
+		
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 64, 0, "Vs Stupid v" + vsStupidVersion, 12);
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);//borrowing fucking code >:]
 		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
