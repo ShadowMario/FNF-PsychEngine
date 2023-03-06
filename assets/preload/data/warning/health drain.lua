a56f41a6fd81b2d408e7a3eed0fe7cbc76aeabb5
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-03-06 15:56:57 -0500

    readdvancement to some health drain

--- assets/preload/data/warning/health drain.lua
+++ assets/preload/data/warning/health drain.lua
@@ -1,6 +1,6 @@
 function opponentNoteHit()
     health = getProperty('health')
-    if getProperty('health') > 0.01 then
-        setProperty('health', health- 0.01);
+    if getProperty('health') > 0.0 then
+        setProperty('health', health- 0.03);
     end
 end
