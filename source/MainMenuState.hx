38107ec0fee224aa1a137c91be5481446b48bd33
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-03-07 01:23:12 -0500

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -129,7 +129,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
                 
-		char1 = new Character(800, -130, 'bf', true)
+		char1 = new Character(800, -130, 'bf', true);
 		char1.setGraphicSize(Std.int(char1.width * 0.8));
 		add(char1);
 		char1.visible = false;
