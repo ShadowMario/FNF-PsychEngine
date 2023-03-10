da5277c085104f8818b503de940d4815e7e9d04b
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-03-10 03:14:49 -0500

    I'm still alive, just busy

--- source/Song.hx
+++ source/Song.hx
@@ -107,27 +107,13 @@ class Song
 			#end
 		}
 
+		if (rawJson == null)
+			throw "Could not find JSON file in: " + formattedFolder; 
+
 		while (!rawJson.endsWith("}"))
 		{
 			rawJson = rawJson.substr(0, rawJson.length - 1);
-			// LOL GOING THROUGH THE BULLSHIT TO CLEAN IDK WHATS STRANGE
 		}
-
-		// FIX THE CASTING ON WINDOWS/NATIVE
-		// Windows???
-		// trace(songData);
-
-		// trace('LOADED FROM JSON: ' + songData.notes);
-		/* 
-			for (i in 0...songData.notes.length)
-			{
-				trace('LOADED FROM JSON: ' + songData.notes[i].sectionNotes);
-				// songData.notes[i].sectionNotes = songData.notes[i].sectionNotes
-			}
-
-				daNotes = songData.notes;
-				daSong = songData.song;
-				daBpm = songData.bpm; */
 
 		var songJson:Dynamic = parseJSONshit(rawJson);
 		if(jsonInput != 'events') StageData.loadDirectory(songJson);
