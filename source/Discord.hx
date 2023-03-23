5d2bfd8e667a34a0fdf07e8b6945e8ff67e7c853
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-03-23 16:27:40 -0400

    minor edits

--- source/Discord.hx
+++ source/Discord.hx
@@ -83,7 +83,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'StupidNor blep',
-			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
+			largeImageText: "Psych Engine Version: " + MainMenuState.psychEngineVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
