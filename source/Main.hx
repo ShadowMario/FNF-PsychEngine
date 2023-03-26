6e1b625df2bf392c4f051960680614f9497fcb47
lemz1 <87707926+lemz1@users.noreply.github.com>
2023-03-26 14:03:59 +0200

    no more deprecaded and version stuff

--- source/Main.hx
+++ source/Main.hx
@@ -141,7 +141,7 @@ class Main extends Sprite
 			}
 		}
 
-		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine\n\n> Crash Handler written by: sqirra-rng";
+		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/ShadowMario/FNF-PsychEngine\n" + #if !officialBuild "Modded " + #end "Game Version: " + states.MainMenuState.psychEngineVersion + "\n\n> Crash Handler written by: sqirra-rng";
 
 		if (!FileSystem.exists("./crash/"))
 			FileSystem.createDirectory("./crash/");
--- source/objects/AchievementPopup.hx
+++ source/objects/AchievementPopup.hx
@@ -33,7 +33,7 @@ class AchievementPopup extends FlxSpriteGroup {
 		add(achievementText);
 		add(achievementIcon);
 
-		var cam:Array<FlxCamera> = FlxCamera.defaultCameras;
+		var cam:Array<FlxCamera> = @:privateAccess FlxCamera._defaultCameras;
 		if(camera != null) {
 			cam = [camera];
 		}
