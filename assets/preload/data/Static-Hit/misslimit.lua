6ed7c0bd28127a66d26a01c3eaaf5975b7d4414f
MyFnf <113801267+LilDrippyMyFnf@users.noreply.github.com>
2023-03-29 16:25:24 -0400

    ok here, static-hit is now ACTUALLY possible with the misslimit

--- assets/preload/data/Static-Hit/misslimit.lua
+++ assets/preload/data/Static-Hit/misslimit.lua
@@ -1,4 +1,4 @@
-local misslimit = 25 -- You can set this to any number you want!
+local misslimit = 50 -- You can set this to any number you want!
 
 function onUpdatePost(elasped)
     setTextString('scoreTxt', 'Score: '.. score .. ' | Misses: '.. misses .. '/'.. misslimit .. ' | Rating: '.. ratingName)
