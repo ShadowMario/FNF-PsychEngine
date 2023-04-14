fc1bc50c2f512538838a9b5bf95de361cd754a90
Idklool <102881579+Its-me-a-guy@users.noreply.github.com>
2023-04-14 08:23:04 -0300

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -91,7 +91,7 @@ class FunkinLua {
 			var resultStr:String = Lua.tostring(lua, result);
 			if(resultStr != null && result != 0) {
 				trace('Error on lua script! ' + resultStr);
-				#if windows
+				#if (windows || mobile)
 				lime.app.Application.current.window.alert(resultStr, 'Error on lua script!');
 				#else
 				luaTrace('Error loading lua script: "$script"\n' + resultStr, true, false, FlxColor.RED);
