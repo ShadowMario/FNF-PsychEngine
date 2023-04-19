a5f8adf54b7ea0ce409e622868b49e9434ac73c7
HackDev <80620538+Hackx2@users.noreply.github.com>
2023-04-19 17:26:54 +0100

    a
    a

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -33,6 +33,13 @@ class MainMenuState extends MusicBeatState
 		'options'
 	];
 
+		
+	private var versionShitInt:Int = 1;
+	private var versionShitArray:Array<Array<Dynamic>> = [// Name, Version, X, Y
+			["Psych Engine v", psychEngineVersion, null, null],
+			["Friday Night Funkin' v", Application.current.meta.get('version'), null, null]
+	];
+
 	var magenta:FlxSprite;
 	var camFollow:FlxObject;
 	var camFollowPos:FlxObject;
@@ -121,14 +128,19 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
-		versionShit.scrollFactor.set();
-		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(versionShit);
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
-		versionShit.scrollFactor.set();
-		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		add(versionShit);
+		versionShitArray.reverse();
+		for (i in 0...versionShitArray.length){// ngl it looks ugly, with all the [i]s
+			if (versionShitArray[i][1] == null) versionShitArray[i][1] = "";
+
+			var versionShit:FlxText = new FlxText(12, FlxG.height - 22 * versionShitInt, 0, versionShitArray[i][0] + versionShitArray[i][1]);
+			versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			versionShit.x += versionShitArray[i][2];
+			versionShit.y -= versionShitArray[i][3];
+			versionShit.scrollFactor.set();
+			add(versionShit);
+
+			versionShitInt++;
+		}
 
 		// NG.core.calls.event.logEvent('swag').send();
 
