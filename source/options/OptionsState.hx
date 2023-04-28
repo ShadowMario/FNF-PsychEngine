9ea5a62b2cd0220f0c2e4a52ce49ebf6e2458584
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-04-28 17:42:46 +0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -70,6 +70,9 @@ class OptionsState extends MusicBeatState
 	var selectorRight:Alphabet;
 
 	override function create() {
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
 		#if desktop
 		DiscordClient.changePresence("Options Menu", null);
 		#end
