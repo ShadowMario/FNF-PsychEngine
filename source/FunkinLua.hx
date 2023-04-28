9f22c27e1f1356cb9c306a97902956facf96574c
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-04-28 17:33:32 +0300

    update source/FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -252,7 +252,7 @@ class FunkinLua {
 		});
 
 		// shader shit
-		Lua_helper.add_callback(lua, "initLuaShader", function(name:String, glslVersion:Int = 120) {
+		Lua_helper.add_callback(lua, "initLuaShader", function(name:String) {
 			if(!ClientPrefs.shaders) return false;
 
 			#if (!flash && MODS_ALLOWED && sys)
@@ -2861,7 +2861,7 @@ class FunkinLua {
 	}
 	#end
 	
-	function initLuaShader(name:String, ?glslVersion:Int = 120)
+	function initLuaShader(name:String)
 	{
 		if(!ClientPrefs.shaders) return false;
 
