1c8b6524ff3702f271fcc74de66d13a271e4a5cb
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-04-28 18:03:48 +0300

    update source/FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -256,7 +256,7 @@ class FunkinLua {
 			if(!ClientPrefs.shaders) return false;
 
 			#if (!flash && MODS_ALLOWED && sys)
-			return initLuaShader(name, glslVersion);
+			return initLuaShader(name, glslesVersion);
 			#else
 			luaTrace("initLuaShader: Platform unsupported for Runtime Shaders!", false, false, FlxColor.RED);
 			#end
