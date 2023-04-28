57501c433049e866cf66d46bcbe58ea8488b185d
Nuno-Filipe-Studios <109983796+Nuno-Filipe-Studios@users.noreply.github.com>
2023-04-28 22:14:58 +0200

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -59,10 +59,10 @@ import sys.io.File;
 #end
 
 
-#if (flixel > "5.3.0")
-@:access(flixel.system.FlxSound._sound)
-#else
+#if (flixel >= "5.3.0")
 @:access(flixel.sound.FlxSound._sound)
+#else
+@:access(flixel.system.FlxSound._sound)
 #end
 @:access(openfl.media.Sound.__buffer)
 
