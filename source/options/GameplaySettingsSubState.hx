eb48dcd71e73250754d8947227461d3d6c7edde4
TechnikTil <89487150+TechnikTil@users.noreply.github.com>
2023-05-25 16:19:36 -0600

    Add the option for Always Updating Camera

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -70,6 +70,13 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 			true);
 		addOption(option);
 
+		var option:Option = new Option('Always Update Camera',
+			'If checked, camera will always update,\nwhich makes the camera more precise.',
+			'alwaysUpdateCamera',
+			'bool',
+			true);
+		addOption(option);
+
 		var option:Option = new Option('Disable Reset Button',
 			"If checked, pressing Reset won't do anything.",
 			'noReset',
