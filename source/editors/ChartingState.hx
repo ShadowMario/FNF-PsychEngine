9feca2e853306f0e7c4ef5123a4ebaf68f6e7f03
JordanSantiagoYT <61796448+JordanSantiagoYT@users.noreply.github.com>
2023-06-06 01:58:13 -0400

    Removed SHIFT+O and SHIFT+P, they didn't work

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -361,8 +361,6 @@ class ChartingState extends MusicBeatState
 		\nHold Alt and click on a note to change it to the selected note type
 		\nZ/X - Zoom in/out
 		\nC - Draw your charts! Easier charting for your Bambi fansongs lmao
-		\nShift + O - Created sections will be for the Opponent
-		\nShift + P - Created sections will be for the Player
 		\n
 		\nEsc - Test your chart inside Chart Editor
 		\nEnter - Play your chart
@@ -372,8 +370,8 @@ class ChartingState extends MusicBeatState
 
 		var tipTextArray:Array<String> = text.split('\n');
 		for (i in 0...tipTextArray.length) {
-			var tipText:FlxText = new FlxText(UI_box.x, UI_box.y + UI_box.height + 8, 0, tipTextArray[i], 18);
-			tipText.y += i * 8;
+			var tipText:FlxText = new FlxText(UI_box.x, UI_box.y + UI_box.height + 8, 0, tipTextArray[i], 16);
+			tipText.y += i * 9;
 			tipText.setFormat(Paths.font("vcr.ttf"), 12, FlxColor.WHITE, LEFT/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
 			//tipText.borderSize = 2;
 			tipText.scrollFactor.set();
