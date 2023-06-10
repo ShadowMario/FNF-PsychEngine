6030c8ddeebf537e9bb179b1fa79694bfd5e8194
Stilic <lateman-jpeg@outlook.fr>
2023-06-10 08:35:11 +0200

    Fix the defaultCameras warning

--- source/Achievements.hx
+++ source/Achievements.hx
@@ -129,7 +129,8 @@ class AchievementObject extends FlxSpriteGroup {
 		add(achievementText);
 		add(achievementIcon);
 
-		var cam:Array<FlxCamera> = FlxCamera.defaultCameras;
+		@:privateAccess
+		var cam:Array<FlxCamera> = FlxCamera._defaultCameras;
 		if(camera != null) {
 			cam = [camera];
 		}
