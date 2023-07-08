8d8af5a0b22eb209da24177f20dbfffcde22a0da
12abz <109289447+12abz@users.noreply.github.com>
2023-07-08 06:58:14 -0500

    Update linuxstup.sh

--- linuxstup.sh
+++ linuxstup.sh
@@ -1,5 +1,14 @@
 #Linux Setup
 sudo apt-get install libvlc-dev libvlccore-dev libvlc-bin vlc-bin haxe g++
+mkdir ~/hxsetup
+cd ~/hxsetup
+wget https://github.com/HaxeFoundation/haxe/releases/download/4.2.5/haxe-4.2.5-linux64.tar.gz
+tar -xf *
+sudo rm /usr/bin/haxe /usr/bin/haxelib
+sudo cp */haxe /usr/bin/
+sudo cp */haxelib /usr/bin
+sudo rm -r /usr/local/haxe/std
+sudo cp -r */std /usr/local/haxe
 mkdir ~/haxelib && haxelib setup ~/haxelib
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
 haxelib install flixel-addons 3.0.2
