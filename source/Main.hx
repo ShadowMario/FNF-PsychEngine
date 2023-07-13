5cd0afbe9292c6e6ece63b7c00f8011d03916010
Sword352 <99792114+Sword352@users.noreply.github.com>
2023-07-13 11:02:19 +0100

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -116,7 +116,7 @@ class Main extends Sprite
 			   for (cam in FlxG.cameras.list) {
 				@:privateAccess
 				if (cam != null && cam._filters != null)
-					resepSpriteCache(cam.flashSprite);
+					resetSpriteCache(cam.flashSprite);
 			   }
 		     }
 
