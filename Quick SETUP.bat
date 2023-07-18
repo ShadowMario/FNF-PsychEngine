de5affe226d6a75ba287bd3a1b0a0800129e04ba
JordanSantiagoYT <61796448+JordanSantiagoYT@users.noreply.github.com>
2023-07-18 04:46:17 -0400

    now you can compile js engine with just 4 steps
    1. download source code
    2. run "Quick SETUP.bat"
    3. open the art folder
    4. run "build_x64"

---
+++ Quick SETUP.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Quickly setting up JS Engine for compiling...
+haxelib install hmm
+haxelib run hmm install
+echo Finished. You may now compile JS Engine!
+echo Press any key to exit.
+pause >nul
