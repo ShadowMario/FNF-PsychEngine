f0dc904a71758350ef4155239ae841b43bf7dad0
JordanSantiagoYT <61796448+JordanSantiagoYT@users.noreply.github.com>
2023-07-21 00:37:10 -0400

    aAAAAAHNH

--- Quick SETUP.bat
+++ Quick SETUP.bat
@@ -1,7 +1,10 @@
 @echo off
 echo Quickly setting up JS Engine for compiling...
 haxelib install hmm
-haxelib run hmm install
+haxelib --global run hmm install
 echo Finished. You may now compile JS Engine!
+echo Oh wait!! Just a little precaution..
+haxelib --global run set hxCodec 2.5.1
+echo There. NOW we're done.
 echo Press any key to exit.
 pause >nul
