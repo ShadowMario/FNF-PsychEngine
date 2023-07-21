f56d6fa0db4397bf332d9e0c03737ac2196fdebc
JordanSantiagoYT <61796448+JordanSantiagoYT@users.noreply.github.com>
2023-07-21 12:32:44 -0400

    Fix Quick SETUP not fully working for some people

--- Quick SETUP.bat
+++ Quick SETUP.bat
@@ -1,10 +1,11 @@
 @echo off
 echo Quickly setting up JS Engine for compiling...
-haxelib install hmm
+haxelib --global install hmm
 haxelib --global run hmm install
 echo Finished. You may now compile JS Engine!
 echo Oh wait!! Just a little precaution..
-haxelib --global run set hxCodec 2.5.1
+haxelib install hxCodec 2.5.1
+haxelib set hxCodec 2.5.1
 echo There. NOW we're done.
 echo Press any key to exit.
 pause >nul
