df403e19834508e07c1633efedb7b7310e93bc0d
Danya20072019 <olgababonina@gmail.com>
2023-07-24 03:52:25 +0300

    Revert "fix"
    This reverts commit ed7a445433de5696f45ef5d98d305e5e4e37dd55.

--- source/states/editors/NoteSplashDebugState.hx
+++ source/states/editors/NoteSplashDebugState.hx
@@ -139,7 +139,6 @@ class NoteSplashDebugState extends MusicBeatState
 		if(controls.BACK && notTyping)
 		{
 			MusicBeatState.switchState(new MasterEditorMenu());
-			FlxG.sound.playMusic(Paths.music('freakyMenu'));
 			FlxG.mouse.visible = false;
 		}
 		super.update(elapsed);
