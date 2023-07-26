a8088193388466fc6e9d06ac246a05fd7614aa11
mcagabe19 <egzozu.be.bas@gmail.com>
2023-07-27 00:54:34 +0300

    annoyin'

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -326,7 +326,7 @@ class StoryMenuState extends MusicBeatState
 				FreeplayState.destroyFreeplayVocals();
 			});
 			
-			#if MODS_ALLOWED
+			#if (MODS_ALLOWED && desktop)
 			DiscordClient.loadModRPC();
 			#end
 		} else {
