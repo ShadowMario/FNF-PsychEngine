da7fd4d87a771dd04a3619a8740683916b94c2f5
Karim Akra <94998581+Karim-Akra@users.noreply.github.com>
2023-07-27 11:03:35 +0300

    Typo

--- source/Main.hx
+++ source/Main.hx
@@ -82,7 +82,7 @@ class Main extends Sprite
 			game.width = Math.ceil(stageWidth / game.zoom);
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
-		SUtil.checkFiles()
+		SUtil.checkFiles();
 	
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
