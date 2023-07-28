66344cdaa501555a78faea6f96161dcd27e4c804
Lily <82843871+mcagabe19@users.noreply.github.com>
2023-07-28 17:17:46 +0300

    Update WeekData.hx

--- source/backend/WeekData.hx
+++ source/backend/WeekData.hx
@@ -97,7 +97,7 @@ class WeekData {
 		var originalLength:Int = directories.length;
 		#end
 
-		var sexList:Array<String> = CoolUtil.coolTextFile(Paths.getPreloadPath('weeks/weekList.txt'));
+		var sexList:Array<String> = CoolUtil.coolTextFile(SUtil.getPath() + Paths.getPreloadPath('weeks/weekList.txt'));
 		for (i in 0...sexList.length) {
 			for (j in 0...directories.length) {
 				var fileToCheck:String = directories[j] + 'weeks/' + sexList[i] + '.json';
