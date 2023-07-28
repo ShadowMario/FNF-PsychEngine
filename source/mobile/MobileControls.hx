b32c23cffb10aaeb1ec6352cd14e810aa43a6bc0
Karim Akra <94998581+Karim-Akra@users.noreply.github.com>
2023-07-28 10:48:04 +0300

    Update MobileControls.hx

--- source/mobile/MobileControls.hx
+++ source/mobile/MobileControls.hx
@@ -97,6 +97,7 @@ class MobileControls extends FlxSpriteGroup
 
 	public static function getMode():Int
 	{
+		instance = this;
 		if (FlxG.save.data.mobileControlsMode == null)
 		{
 			FlxG.save.data.mobileControlsMode = 0;
