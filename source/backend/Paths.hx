c970288395cc2eb78d797afa803a55a27d6fa6de
Lily <82843871+mcagabe19@users.noreply.github.com>
2023-07-29 02:02:00 +0300

    so I think

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -412,12 +412,12 @@ class Paths
 		}
 		#end
 		// I hate this so god damn much
-		var gottenPath:String = /*SUtil.getPath() +*/ getPath('$path/$key.$SOUND_EXT', SOUND, library);
+		var gottenPath:String = SUtil.getPath() + getPath('$path/$key.$SOUND_EXT', SOUND, library);
 		gottenPath = gottenPath.substring(gottenPath.indexOf(':') + 1, gottenPath.length);
 		// trace(gottenPath);
 		if(!currentTrackedSounds.exists(gottenPath))
-		#if (MODS_ALLOWED && !mobile)
-			currentTrackedSounds.set(gottenPath, Sound.fromFile('./' + gottenPath));
+		#if MODS_ALLOWED
+			currentTrackedSounds.set(gottenPath, Sound.fromFile(#if !mobile './' + #end gottenPath));
 		#else
 		{
 			var folder:String = '';
