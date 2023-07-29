578a35df956212e5a2b37febbfd9a5107afb64c8
mcagabe19 <egzozu.be.bas@gmail.com>
2023-07-29 15:37:21 +0300

    Revert "Update Main.hx"
    This reverts commit 74fa7b06bd3a43818d2e4687460a3cebaa363911.

--- source/Main.hx
+++ source/Main.hx
@@ -84,11 +84,11 @@ class Main extends Sprite
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
-		/*#if mobile
+		#if mobile
 		addChild(new FlxGame(1280, 720, TitleState, 60, 60, true, false));
-		#else*/
+		#else
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
-		//#end
+		#end
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
