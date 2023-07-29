56ae14296d4247b5b33aef48e12a686861366cb1
Lily <82843871+mcagabe19@users.noreply.github.com>
2023-07-29 16:58:46 +0300

    Update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -219,7 +219,7 @@ class BaseOptionsMenu extends MusicBeatSubstate
 				}
 			}
 
-			if(controls.RESET #if mobileC || virtualPad.buttonC.justPressed #end)
+			if(controls.RESET #if mobileC || MusicBeatSubstate.virtualPad.buttonC.justPressed #end)
 			{
 				var leOption:Option = optionsArray[curSelected];
 				leOption.setValue(leOption.defaultValue);
