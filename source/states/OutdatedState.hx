6686aef4046a7985689858b3e0b7bbe62aecdbdc
Lily <82843871+mcagabe19@users.noreply.github.com>
2023-07-30 10:02:13 +0300

    I'm silly

--- source/states/OutdatedState.hx
+++ source/states/OutdatedState.hx
@@ -14,21 +14,21 @@ class OutdatedState extends MusicBeatState
 
 		#if mobileC
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Sup bro, looks like you're running an   \n
-			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
-			please update to " + TitleState.updateVersion + "!\n
-			Press ESCAPE to proceed anyway.\n
-			\n
-			Thank you for using the Engine!",
-			32);
-		#else
-		warnText = new FlxText(0, 0, FlxG.width,
 			"Sup kiddo, looks like you're running an   \n
 			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			Press B to proceed anyway.\n
 			\n
 			Thank you for using the Port!",
+			32);
+		#else
+		warnText = new FlxText(0, 0, FlxG.width,
+			"Sup bro, looks like you're running an   \n
+			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
+			please update to " + TitleState.updateVersion + "!\n
+			Press ESCAPE to proceed anyway.\n
+			\n
+			Thank you for using the Engine!",
 			32);
 		#end
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
