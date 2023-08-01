4d46a7d1563a62f1efba9b0e78527373a9e195a4
Karim Akra <karimakra878@gmail.com>
2023-08-01 20:31:05 +0300

    why not?

--- source/backend/SUtil.hx
+++ source/backend/SUtil.hx
@@ -33,7 +33,10 @@ class SUtil
 	public static function getPath():String
 	{
 		#if android
-		return  Context.getExternalFilesDir(null) + '/';
+		if (VERSION.SDK_INT <= 30) //if is android 11 and above, the storage directory will be android/data/com.shadowmario.psychengine
+			return  Context.getExternalFilesDir(null) + '/';
+		else if (VERSION.SDK_INT >= 29) //if is android 10 and lower, the storage directory will be /.PsychEngine/
+			return Environment.getExternalStorageDirectory() + '/' + '.' + Lib.application.meta.get('file') + '/;'
 		#elseif ios
 		return LimeSystem.applicationStorageDirectory;
 		#end
