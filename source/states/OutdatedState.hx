0000109f5a5db6711d193198de81df2110a76071
Lily <82843871+mcagabe19@users.noreply.github.com>
2023-08-03 14:09:41 +0300

    Update OutdatedState.hx

--- source/states/OutdatedState.hx
+++ source/states/OutdatedState.hx
@@ -45,7 +45,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/mcagabe19/FNF-PsychEngine-Mobile/actions");
+				CoolUtil.browserLoad("https://github.com/mcagabe19/FNF-PsychEngine-Mobile/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
