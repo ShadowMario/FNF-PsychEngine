9735cd7ab275c0b100c862518de5d52db9fae486
Karim Akra <karimakra878@gmail.com>
2023-08-03 03:00:56 +0300

    sutil fix

--- source/backend/SUtil.hx
+++ source/backend/SUtil.hx
@@ -34,12 +34,7 @@ class SUtil
 	public static function getPath():String
 	{
 		#if android
-		if (VERSION.SDK_INT >= 30) //if is android 11 and above, the storage directory will be android/data/com.shadowmario.psychengine
-			return Context.getExternalFilesDir(null) + '/';
-		else if (VERSION.SDK_INT <= 29) //if is android 10 and lower, the storage directory will be /.PsychEngine/
-			return Environment.getExternalStorageDirectory() + '/' + '.' + Lib.application.meta.get('file') + '/';
-		else
-			return Context.getExternalFilesDir(null) + '/'; // I FUCKING HATE HAXE
+		return Context.getExternalFilesDir(null) + '/';
 		#elseif ios
 		return LimeSystem.applicationStorageDirectory;
 		#end
@@ -52,11 +47,10 @@ class SUtil
 	{
 		#if android
 		if (!Permissions.getGrantedPermissions().contains(Permissions.WRITE_EXTERNAL_STORAGE)
-			&& !Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE) && !Permissions.getGrantedPermissions().contains(Permissions.MANAGE_EXTERNAL_STORAGE))
+			&& !Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE))
 		{
 				Permissions.requestPermissions(Permissions.WRITE_EXTERNAL_STORAGE);
 				Permissions.requestPermissions(Permissions.READ_EXTERNAL_STORAGE);
-				Permissions.requestPermissions(Permissions.MANAGE_EXTERNAL_STORAGE);
 				Lib.application.window.alert('This game need external storage access to function properly' + "\nTo give it access you must accept the storage permission\nIf you accepted you're good to go!\nIf not you'll face issues inGame..."
 					+ '\nPress Ok to see what happens',
 					'Permissions?');
@@ -66,21 +60,14 @@ class SUtil
 			{
 				try {
 				if (Permissions.getGrantedPermissions().contains(Permissions.WRITE_EXTERNAL_STORAGE)
-					&& Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE) && !Permissions.getGrantedPermissions().contains(Permissions.MANAGE_EXTERNAL_STORAGE))
+					&& Permissions.getGrantedPermissions().contains(Permissions.READ_EXTERNAL_STORAGE))
 				{
 					if (!FileSystem.exists(SUtil.getPath()))
 						FileSystem.createDirectory(SUtil.getPath());
-					if (FileSystem.exists(Environment.getDownloadCacheDirectory() + "assets"))
-						copyDirectoryFromExternalStorage(Environment.getDownloadCacheDirectory() + "assets", SUtil.getPath());
-	
-					if (FileSystem.exists(Environment.getDownloadCacheDirectory() + "mods"))
-						copyDirectoryFromExternalStorage(Environment.getDownloadCacheDirectory() + "mods", SUtil.getPath());
 	
 				}
 			} 
 			catch(e:Dynamic) {
-			Lib.application.window.alert("Whoops, your game have failed in copying the needed files!\n Please make sure you have the needed folders:\nmods and assets in your downloads folder.\n if the problem presists, try moving the files manualy into " + SUtil.getPath + " directory!",
-			'Missing files!');
 			Toast.makeText('Error!\nCouldn\'t copy the files because:\n' + e, Toast.LENGTH_LONG);
 			//LimeSystem.exit(1);
 			}
@@ -89,44 +76,14 @@ class SUtil
 		#if mobile
 		if (!FileSystem.exists(SUtil.getPath() +  'assets') && !FileSystem.exists(SUtil.getPath() +  'mods'))
 		{
-			Lib.application.window.alert("Whoops, seems like you didn't extract the files from the .APK!\nPlease copy the files from the .APK to\n" + SUtil.getPath(),
-				'Error!');
-			LimeSystem.exit(1);
+			Lib.application.window.alert("The game cannot locate any of the needed files.\nPlease try copying the files from the installed .APK into\n" + SUtil.getPath() + "\nIf you already did please review where you put the files.",
+				'Missing Files!');
+				LimeSystem.exit(1);
 		}
-		else if ((FileSystem.exists(SUtil.getPath() +  'assets') && !FileSystem.isDirectory(SUtil.getPath() + 'assets'))
-			&& (FileSystem.exists(SUtil.getPath() +  'mods') && !FileSystem.isDirectory(SUtil.getPath() + 'mods')))
+		else if ((FileSystem.exists(SUtil.getPath() +  'assets') && !FileSystem.isDirectory(SUtil.getPath() + 'assets')))
 		{
-			Lib.application.window.alert("Why did you create two files called assets and mods instead of copying the folders from the .APK?, expect a crash.",
-				'Error!');
-			LimeSystem.exit(1);
-		}
-		else
-		{
-			if (!FileSystem.exists(SUtil.getPath() +  'assets'))
-			{
-				Lib.application.window.alert("Whoops, seems like you didn't extract the assets/assets folder from the .APK!\nPlease copy the assets/assets folder from the .APK to\n" + SUtil.getPath(),
-					'Error!');
-				LimeSystem.exit(1);
-			}
-			else if (FileSystem.exists(SUtil.getPath() +  'assets') && !FileSystem.isDirectory(SUtil.getPath() + 'assets'))
-			{
-				Lib.application.window.alert("Why did you create a file called assets instead of copying the assets directory from the .APK?, expect a crash.",
-					'Error!');
-				LimeSystem.exit(1);
-			}
-
-			if (!FileSystem.exists(SUtil.getPath() +  'mods'))
-			{
-				Lib.application.window.alert("Whoops, seems like you didn't extract the assets/mods folder from the .APK!\nPlease copy the assets/mods folder from the .APK to\n" + SUtil.getPath(),
-					'Error!');
-				LimeSystem.exit(1);
-			}
-			else if (FileSystem.exists(SUtil.getPath() +  'mods') && !FileSystem.isDirectory(SUtil.getPath() + 'mods'))
-			{
-				Lib.application.window.alert("Why did you create a file called mods instead of copying the mods directory from the .APK?, expect a crash.",
-					'Error!');
-				LimeSystem.exit(1);
-			}
+			Lib.application.window.alert("The game reads that there are no files in the assets folder you got in\n" + SUtil.getPath()+"assets\nPlease copy them from the installed .APK file.",
+				'Could Not read the directory.');
 		}
 		#end
 	}
@@ -231,7 +188,7 @@ class SUtil
 		}
 	}
 
-	public static function copyContentFromInternalStorage(copyPath:String, savePath:String):Void
+	public static function copyContent(copyPath:String, savePath:String):Void
 	{
 		try
 		{
@@ -252,49 +209,5 @@ class SUtil
 			#end
 		}
 	}
-
-	public static function copyContentFromExternalStorage(copyPath:String, savePath:String):Void
-		{
-			try
-			{
-				if (!FileSystem.exists(savePath) && FileSystem.exists(copyPath))
-				{
-					if (!FileSystem.exists(Path.directory(savePath)))
-						SUtil.mkDirs(Path.directory(savePath));
-	
-					File.saveBytes(savePath, File.getBytes(copyPath));
-				}
-			}
-			catch (e:Dynamic)
-			{
-				#if (android && debug)
-				Toast.makeText('Error!\nClouldn\'t copy the $copyPath because:\n' + e, Toast.LENGTH_LONG);
-				#else
-				LimeLogger.println('Error!\nClouldn\'t copy the $copyPath because:\n' + e);
-				#end
-			}
-		}
-		//i readed that File.copy() copies directories so idk
-		public static function copyDirectoryFromExternalStorage(copyPath:String, savePath:String):Void
-			{
-				try
-				{
-					if (!FileSystem.exists(savePath) && FileSystem.exists(copyPath))
-					{
-						if (!FileSystem.exists(Path.directory(savePath)))
-							FileSystem.createDirectory(Path.directory(savePath));
-			
-						File.copy(copyPath, savePath);
-					}
-				}
-				catch (e:Dynamic)
-				{
-					#if (android && debug)
-					Toast.makeText('Error!\nCouldn\'t copy the $copyPath because:\n' + e, Toast.LENGTH_LONG);
-					#else
-					LimeLogger.println('Error!\nCouldn\'t copy the $copyPath because:\n' + e);
-					#end
-				}
-			}
 	#end
 }
