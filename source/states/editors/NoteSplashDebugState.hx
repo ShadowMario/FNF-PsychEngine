3b0fcd3871f38d0e571f975af4e786a816bbe473
Lily <82843871+mcagabe19@users.noreply.github.com>
2023-08-05 12:48:10 +0300

    Update NoteSplashDebugState.hx

--- source/states/editors/NoteSplashDebugState.hx
+++ source/states/editors/NoteSplashDebugState.hx
@@ -307,9 +307,12 @@ class NoteSplashDebugState extends MusicBeatState
 
 		var pathSplit:Array<String> = (Paths.getPath('images/$texturePath.png', IMAGE, true).split('.png')[0] + '.txt').split(':');
 		var path:String = pathSplit[pathSplit.length-1].trim();
+		#if desktop
 		savedText.text = 'Saved to: $path';
 		sys.io.File.saveContent(path, strToSave);
-
+		#else
+		SUtil.saveContent(path, "", strToSave);
+		#end
 		//trace(strToSave);
 		#else
 		savedText.text = 'Can\'t save on this platform, too bad.';
