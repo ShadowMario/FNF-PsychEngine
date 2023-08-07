dcf68ae1a28d360937d95243981708d30a8a97ce
Gabo137 <108769155+Gabo137@users.noreply.github.com>
2023-08-07 15:00:50 -0300

    #if (!macro) added
    yeah

--- source/import.hx
+++ source/import.hx
@@ -1,15 +1,20 @@
 
 //Discord API
+#if (!macro)
 #if desktop
 import backend.Discord;
 #end
+#end
 
 //Psych
+#if (!macro)
 #if LUA_ALLOWED
 import llua.*;
 import llua.Lua;
 #end
+#end
 
+#if (!macro)
 import backend.Paths;
 import backend.Controls;
 import backend.CoolUtil;
@@ -21,19 +26,29 @@ import backend.Conductor;
 import backend.BaseStage;
 import backend.Difficulty;
 import backend.Mods;
+#end
 
+#if (!macro)
 import objects.Alphabet;
 import objects.BGSprite;
+#end
 
+#if (!macro)
 import states.PlayState;
 import states.LoadingState;
+#end
 
 //Flixel
 #if (flixel >= "5.3.0")
+#if (!macro)
 import flixel.sound.FlxSound;
-#else
+#else#if (!macro)
 import flixel.system.FlxSound;
 #end
+#end
+#end
+
+#if (!macro)
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
@@ -45,5 +60,6 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
+#end
 
 using StringTools;
