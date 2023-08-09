43004b3f146f3ae3b54faf3ab9cebb90a3504803
Lily <82843871+mcagabe19@users.noreply.github.com>
2023-08-09 16:36:05 +0300

    always check

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -19,16 +19,14 @@ class CoolUtil
 	inline public static function capitalize(text:String)
 		return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
 
-	inline public static function coolTextFile(path:String, sutil:Bool = true):Array<String>
+	inline public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:String = null;
 		#if (sys && MODS_ALLOWED)
 		var formatted:Array<String> = path.split(':'); //prevent "shared:", "preload:" and other library names on file path
 		path = formatted[formatted.length-1];
-		if (sutil) 
-			if(FileSystem.exists(SUtil.getPath() + path)) daList = File.getContent(SUtil.getPath() + path);
-		if (!sutil)
-			if(FileSystem.exists(path)) daList = File.getContent(path);
+		if(FileSystem.exists(SUtil.getPath() + path)) daList = File.getContent(SUtil.getPath() + path);
+		else if(FileSystem.exists(path)) daList = File.getContent(path);
 		#else
 		if(Assets.exists(path)) daList = Assets.getText(path);
 		#end
