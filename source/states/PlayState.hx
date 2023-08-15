560d5ac841b905a81e1a4166d8e66b720603e28b
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-08-15 01:25:49 -0400

    bruh

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2917,7 +2917,7 @@ class PlayState extends MusicBeatState
 					}
 				}
 			}
-			if (!note.isSustainNote && cpuControlled)
+			if (!note.isSustainNote && !cpuControlled)
 			{
 				songScore += 500 * Std.int(healthGain);
 				if(combo > 9999) combo = 9999;
