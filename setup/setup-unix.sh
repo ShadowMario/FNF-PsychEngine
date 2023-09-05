7d4c427f866673eb2533e8cf0678f5f45cff4c1f
BeastlyGabi <45212377+CrowPlexus@users.noreply.github.com>
2023-09-04 22:38:54 -0400

    fix haxe 4.3+ and add new compile scripts
    Co-Authored-By: Rudyrue <73214127+Rudyrue@users.noreply.github.com>

---
+++ setup/setup-unix.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# SETUP FOR MAC AND LINUX SYSTEMS!!!
+#
+# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
+# https://haxe.org/download/version/4.2.5/
+haxe -cp ./setup -D analyzer-optimize -main Main --interp
