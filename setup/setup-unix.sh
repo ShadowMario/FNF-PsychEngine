659032c58ec4b93a1b8f26794d6bebe1298a985b
BeastlyGabi <45212377+CrowPlexus@users.noreply.github.com>
2023-09-07 15:12:17 -0400

    modify setup files so they directly install libraries

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -3,4 +3,13 @@
 #
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
 # https://haxe.org/download/version/4.2.5/
-haxe -cp ./setup -D analyzer-optimize -main Main --interp
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib install tjson
+haxelib install flixel-addons
+haxelib install flixel-tools
+haxelib install SScript
+haxelib install hxCodec
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
