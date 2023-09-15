865e25904070be1145937a037cb5e71c05d71362
Karim-Akra <94998581+Karim-Akra@users.noreply.github.com>
2023-09-15 11:15:01 +0300

    Revert "Not needed damn"
    This reverts commit 1f42be3e64265ae146fd0f2ef08fa4d182027363.

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -432,7 +432,7 @@ class Paths
 
 	#if MODS_ALLOWED
 	inline static public function mods(key:String = '') {
-		return 'mods/' + key;
+		return #if mobile Sys.getCwd() + #end 'mods/' + key;
 	}
 
 	inline static public function modsFont(key:String) {
@@ -490,7 +490,7 @@ class Paths
 			if(FileSystem.exists(fileToCheck))
 				return fileToCheck;
 		}
-		return 'mods/' + key;
+		return #if mobile Sys.getCwd() + #end 'mods/' + key;
 	}
 	#end
 }
