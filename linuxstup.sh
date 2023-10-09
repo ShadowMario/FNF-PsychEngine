536258be4eab3bde2c77503667b45feeb822fd50
12abz <109289447+12abz@users.noreply.github.com>
2023-10-09 17:16:59 -0500

    Update linuxstup.sh

--- linuxstup.sh
+++ linuxstup.sh
@@ -10,15 +10,5 @@ sudo cp */haxelib /usr/bin
 sudo rm -r /usr/local/haxe/std
 sudo cp -r */std /usr/local/haxe
 mkdir ~/haxelib && haxelib setup ~/haxelib
-haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
-haxelib --never install flixel-addons 3.0.2
-haxelib --never install flixel-tools 1.5.1
-haxelib --never install flixel-ui 2.5.0
-haxelib --never install flixel 5.2.2
-haxelib --never install hscript 2.5.0
-haxelib --never install hxCodec 2.5.1
-haxelib --never install lime 8.0.1
-haxelib run lime setup
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
-haxelib --never install openfl 9.2.1
+haxelib run hmm install
 #Script For Psych Engine
