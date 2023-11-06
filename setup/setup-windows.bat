d548f252ff438e32e70fe3415bfa1e0e13ebb987
⍚~Nex <87421482+NexIsDumb@users.noreply.github.com>
2023-11-06 23:42:52 +0100

    dev branch

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -9,9 +9,9 @@ haxelib install flixel
 haxelib install tjson
 haxelib install flixel-addons
 haxelib install flixel-tools
-haxelib install flxanimate
 haxelib install SScript
 haxelib install hxCodec
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
