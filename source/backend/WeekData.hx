c422c58939874fc776a3b4c8b122ed75986040bb
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-11-08 08:18:22 +0200

    update source/backend/WeekData.hx

--- source/backend/WeekData.hx
+++ source/backend/WeekData.hx
@@ -87,13 +87,13 @@ class WeekData {
 		weeksList = [];
 		weeksLoaded.clear();
 		#if MODS_ALLOWED
-		var directories:Array<String> = [Paths.mods(), Paths.getPreloadPath()];
+		var directories:Array<String> = [Paths.mods(), SUtil.getPath() + Paths.getPreloadPath()];
 		var originalLength:Int = directories.length;
 
 		for (mod in Mods.parseList().enabled)
 			directories.push(Paths.mods(mod + '/'));
 		#else
-		var directories:Array<String> = [Paths.getPreloadPath()];
+		var directories:Array<String> = [SUtil.getPath() + Paths.getPreloadPath()];
 		var originalLength:Int = directories.length;
 		#end
 
