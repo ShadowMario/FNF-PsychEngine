98dc6792f448ddb463c58aa3d787d4cbbf665f16
JordanSantiagoYT <jordan.doesosu2@gmail.com>
2023-11-24 13:32:29 -0500

    forgetting something?

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,7 +26,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineJSVersion:String = '1.15.0'; //This is also used for Discord RPC
+	public static var psychEngineJSVersion:String = '1.15.1'; //This is also used for Discord RPC
 	public static var psychEngineVersion:String = '0.6.3'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
