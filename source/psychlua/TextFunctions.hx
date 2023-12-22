a03d53b00d68323fc2195ed944639659cc82081a
ShadowMario <44785097+ShadowMario@users.noreply.github.com>
2023-12-22 12:28:54 -0300

    setTextHeight and setTextAutoSize

--- source/psychlua/TextFunctions.hx
+++ source/psychlua/TextFunctions.hx
@@ -47,6 +47,26 @@ class TextFunctions
 			FunkinLua.luaTrace("setTextWidth: Object " + tag + " doesn't exist!", false, false, FlxColor.RED);
 			return false;
 		});
+		Lua_helper.add_callback(lua, "setTextHeight", function(tag:String, height:Float) {
+			var obj:FlxText = LuaUtils.getTextObject(tag);
+			if(obj != null)
+			{
+				obj.fieldHeight = height;
+				return true;
+			}
+			FunkinLua.luaTrace("setTextHeight: Object " + tag + " doesn't exist!", false, false, FlxColor.RED);
+			return false;
+		});
+		Lua_helper.add_callback(lua, "setTextAutoSize", function(tag:String, value:Bool) {
+			var obj:FlxText = LuaUtils.getTextObject(tag);
+			if(obj != null)
+			{
+				obj.autoSize = value;
+				return true;
+			}
+			FunkinLua.luaTrace("setTextAutoSize: Object " + tag + " doesn't exist!", false, false, FlxColor.RED);
+			return false;
+		});
 		Lua_helper.add_callback(lua, "setTextBorder", function(tag:String, size:Float, color:String, ?style:String = 'outline') {
 			var obj:FlxText = LuaUtils.getTextObject(tag);
 			if(obj != null)
