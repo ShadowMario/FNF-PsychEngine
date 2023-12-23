a96778ab82d6ef3108daa340e21073b4d3ac8cd4
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-12-23 17:45:56 +0200

    Same here

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -14,4 +14,4 @@ haxelib install hxCodec
 haxelib install tjson
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib install hxdiscord_rpc
