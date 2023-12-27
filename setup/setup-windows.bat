199797e5b890f52053ce9a599ae60e47ee632f7a
EliteMasterEric <ericmyllyoja@gmail.com>
2023-12-27 04:30:08 -0500

    Port setup scripts to use hmm

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -3,17 +3,7 @@ color 0a
 cd ..
 @echo on
 echo Installing dependencies.
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
-haxelib install flixel-tools
-haxelib install SScript
-haxelib install hxCodec
-haxelib install tjson
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib install hmm
+haxelib run hmm install
 echo Finished!
 pause
