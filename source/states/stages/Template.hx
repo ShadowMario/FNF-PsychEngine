cbbe88fca28d3659c21e05dc5000091fa31d0fc5
KELOTOMU <miguelmanalo2011@gmail.com>
2024-02-19 18:10:47 +0800

    Update Template.hx

--- source/states/stages/Template.hx
+++ source/states/stages/Template.hx
@@ -25,7 +25,7 @@ class Template extends BaseStage
 	}
 
 	
-	override function countdownTick(count:BaseStage.Countdown, num:Int)
+	override function countdownTick(count:Countdown, num:Int)
 	{
 		switch(count)
 		{
