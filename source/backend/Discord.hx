d1b0d9f45d39a7b05c9b4c2fbb90cdc3c58e5b2f
rich <87835336+richTrash21@users.noreply.github.com>
2024-04-22 19:00:54 +0400

    reflectable presence wow

--- source/backend/Discord.hx
+++ source/backend/Discord.hx
@@ -1,17 +1,21 @@
 package backend;
 
+#if DISCORD_ALLOWED
 import Sys.sleep;
+import sys.thread.Thread;
 import lime.app.Application;
+
 import hxdiscord_rpc.Discord;
 import hxdiscord_rpc.Types;
-import sys.thread.Thread;
+
+import flixel.util.FlxStringUtil;
 
 class DiscordClient
 {
 	public static var isInitialized:Bool = false;
 	private inline static final _defaultID:String = "863222024192262205";
 	public static var clientID(default, set):String = _defaultID;
-	private static var presence:DiscordRichPresence = DiscordRichPresence.create();
+	private static var presence:DiscordPresence = new DiscordPresence();
 	// hides this field from scripts and reflection in general
 	@:unreflective private static var __thread:Thread;
 
@@ -31,12 +35,14 @@ class DiscordClient
 		});
 	}
 
-	public dynamic static function shutdown() {
+	public dynamic static function shutdown()
+	{
 		isInitialized = false;
 		Discord.Shutdown();
 	}
 	
-	private static function onReady(request:cpp.RawConstPointer<DiscordUser>):Void {
+	private static function onReady(request:cpp.RawConstPointer<DiscordUser>):Void
+	{
 		final user = cast (request[0].username, String);
 		final discriminator = cast (request[0].discriminator, String);
 
@@ -50,11 +56,13 @@ class DiscordClient
 		changePresence();
 	}
 
-	private static function onError(errorCode:Int, message:cpp.ConstCharStar):Void {
+	private static function onError(errorCode:Int, message:cpp.ConstCharStar):Void
+	{
 		trace('Discord: Error ($errorCode: ${cast(message, String)})');
 	}
 
-	private static function onDisconnected(errorCode:Int, message:cpp.ConstCharStar):Void {
+	private static function onDisconnected(errorCode:Int, message:cpp.ConstCharStar):Void
+	{
 		trace('Discord: Disconnected ($errorCode: ${cast(message, String)})');
 	}
 
@@ -96,24 +104,28 @@ class DiscordClient
 		if (hasStartTimestamp) startTimestamp = Date.now().getTime();
 		if (endTimestamp > 0) endTimestamp = startTimestamp + endTimestamp;
 
-		presence.details = details;
 		presence.state = state;
+		presence.details = details;
+		presence.smallImageKey = smallImageKey;
 		presence.largeImageKey = largeImageKey;
 		presence.largeImageText = "Engine Version: " + states.MainMenuState.psychEngineVersion;
-		presence.smallImageKey = smallImageKey;
 		// Obtained times are in milliseconds so they are divided so Discord can use it
 		presence.startTimestamp = Std.int(startTimestamp / 1000);
 		presence.endTimestamp = Std.int(endTimestamp / 1000);
 		updatePresence();
 
-		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
+		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp, $largeImageKey');
 	}
 
 	public static function updatePresence()
-		Discord.UpdatePresence(cpp.RawConstPointer.addressOf(presence));
+	{
+		Discord.UpdatePresence(cpp.RawConstPointer.addressOf(presence.__presence));
+	}
 	
-	public static function resetClientID()
+	inline public static function resetClientID()
+	{
 		clientID = _defaultID;
+	}
 
 	private static function set_clientID(newID:String)
 	{
@@ -142,9 +154,9 @@ class DiscordClient
 	#end
 
 	#if LUA_ALLOWED
-	public static function addLuaCallbacks(lua:State) {
+	public static function addLuaCallbacks(lua:State)
+	{
 		Lua_helper.add_callback(lua, "changeDiscordPresence", changePresence);
-
 		Lua_helper.add_callback(lua, "changeDiscordClientID", function(?newID:String) {
 			if(newID == null) newID = _defaultID;
 			clientID = newID;
@@ -152,3 +164,106 @@ class DiscordClient
 	}
 	#end
 }
+
+@:allow(backend.DiscordClient)
+private final class DiscordPresence
+{
+	public var state(get, set):String;
+	public var details(get, set):String;
+	public var smallImageKey(get, set):String;
+	public var largeImageKey(get, set):String;
+	public var largeImageText(get, set):String;
+	public var startTimestamp(get, set):Int;
+	public var endTimestamp(get, set):Int;
+
+	@:noCompletion private var __presence:DiscordRichPresence;
+
+	function new()
+	{
+		__presence = DiscordRichPresence.create();
+	}
+
+	public function toString():String
+	{
+		return FlxStringUtil.getDebugString([
+			LabelValuePair.weak("state", state),
+			LabelValuePair.weak("details", details),
+			LabelValuePair.weak("smallImageKey", smallImageKey),
+			LabelValuePair.weak("largeImageKey", largeImageKey),
+			LabelValuePair.weak("largeImageText", largeImageText),
+			LabelValuePair.weak("startTimestamp", startTimestamp),
+			LabelValuePair.weak("endTimestamp", endTimestamp)
+		]);
+	}
+
+	@:noCompletion inline function get_state():String
+	{
+		return __presence.state;
+	}
+
+	@:noCompletion inline function set_state(value:String):String
+	{
+		return __presence.state = value;
+	}
+
+	@:noCompletion inline function get_details():String
+	{
+		return __presence.details;
+	}
+
+	@:noCompletion inline function set_details(value:String):String
+	{
+		return __presence.details = value;
+	}
+
+	@:noCompletion inline function get_smallImageKey():String
+	{
+		return __presence.smallImageKey;
+	}
+
+	@:noCompletion inline function set_smallImageKey(value:String):String
+	{
+		return __presence.smallImageKey = value;
+	}
+
+	@:noCompletion inline function get_largeImageKey():String
+	{
+		return __presence.largeImageKey;
+	}
+	
+	@:noCompletion inline function set_largeImageKey(value:String):String
+	{
+		return __presence.largeImageKey = value;
+	}
+
+	@:noCompletion inline function get_largeImageText():String
+	{
+		return __presence.largeImageText;
+	}
+
+	@:noCompletion inline function set_largeImageText(value:String):String
+	{
+		return __presence.largeImageText = value;
+	}
+
+	@:noCompletion inline function get_startTimestamp():Int
+	{
+		return __presence.startTimestamp;
+	}
+
+	@:noCompletion inline function set_startTimestamp(value:Int):Int
+	{
+		return __presence.startTimestamp = value;
+	}
+
+	@:noCompletion inline function get_endTimestamp():Int
+	{
+		return __presence.endTimestamp;
+	}
+
+	@:noCompletion inline function set_endTimestamp(value:Int):Int
+	{
+		return __presence.endTimestamp = value;
+	}
+}
+#end
