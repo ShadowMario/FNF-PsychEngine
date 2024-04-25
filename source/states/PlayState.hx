2f9bbe23b7d5668e5681af105053192b7b1c6d20
Meow <103583353+xenkap@users.noreply.github.com>
2024-04-26 01:59:04 +0800

    remove combo/rating popup sprites from comboGroup
    + SScript compilation fix for Tea/TeaCall

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2579,6 +2579,7 @@ class PlayState extends MusicBeatState
 				onComplete: function(tween:FlxTween)
 				{
 					numScore.destroy();
+					comboGroup.remove(numScore);
 				},
 				startDelay: Conductor.crochet * 0.002 / playbackRate
 			});
@@ -2596,6 +2597,8 @@ class PlayState extends MusicBeatState
 			{
 				comboSpr.destroy();
 				rating.destroy();
+				comboGroup.remove(comboSpr);
+				comboGroup.remove(rating);
 			},
 			startDelay: Conductor.crochet * 0.002 / playbackRate
 		});
