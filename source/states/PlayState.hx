9dbd4043b15409d5e745f8cbcf49794c7102b642
Lelmaster3457 <63193722+Lelmaster3457@users.noreply.github.com>
2024-06-16 12:10:13 -0400

    Added video callback

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -859,6 +859,7 @@ class PlayState extends MusicBeatState
 			video.playVideo(filepath);
 			video.finishCallback = function()
 			{
+				callOnLuas('onVideoCompleted', [name]);
 				startAndEnd();
 				return;
 			}
