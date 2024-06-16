9ec6c551c0b9b7813c49a912a2394fe362d509c5
Pumpsuki <71671417+PartnerCapPikachu@users.noreply.github.com>
2024-06-16 11:57:17 -0400

    Update SustainSplash.hx

--- source/objects/SustainSplash.hx
+++ source/objects/SustainSplash.hx
@@ -2,17 +2,24 @@ package objects;
 
 class SustainSplash extends FlxSprite {
 
+  public static var curSplash:Int = 0;
   public static var startCrochet:Float;
   public static var frameRate:Int;
+  public var destroyTimer:FlxTimer;
 
   public function new():Void {
+
     super();
+
     frames = Paths.getSparrowAtlas('noteSplashes/holdSplashes/holdSplash');
     animation.addByPrefix('hold', 'hold', 24, true);
     animation.addByPrefix('end', 'end', 24, false);
     animation.play('hold', true, false, 0);
     animation.curAnim.frameRate = frameRate;
     animation.curAnim.looped = true;
+
+    destroyTimer = new FlxTimer();
+
   }
 
   public function setupSusSplash(strum:StrumNote, daNote:Note, ?playbackRate:Float = 1):Void {
@@ -22,6 +29,8 @@ class SustainSplash extends FlxSprite {
     final timeThingy:Float = (startCrochet * lengthToGet + (timeToGet - Conductor.songPosition + ClientPrefs.data.ratingOffset)) / playbackRate * .001;
 
     var tailEnd:Note = !daNote.isSustainNote ? daNote.tail[daNote.tail.length - 1] : daNote.parent.tail[daNote.parent.tail.length - 1];
+
+    tailEnd.extraData['holdSplash'] = this;
 
     clipRect = new flixel.math.FlxRect(0, !PlayState.isPixelStage ? 0 : -210, frameWidth, frameHeight);
 
@@ -36,7 +45,7 @@ class SustainSplash extends FlxSprite {
     setPosition(strum.x, strum.y);
     offset.set(PlayState.isPixelStage ? 112.5 : 106.25, 100);
 
-    new FlxTimer().start(timeThingy, (idk:FlxTimer) -> {
+    destroyTimer.start(timeThingy, (idk:FlxTimer) -> {
       if (tailEnd.mustPress && !(daNote.isSustainNote ? daNote.parent.noteSplashData.disabled : daNote.noteSplashData.disabled) && ClientPrefs.data.splashAlpha != 0) {
         alpha = ClientPrefs.data.splashAlpha;
         animation.play('end', true, false, 0);
@@ -44,13 +53,26 @@ class SustainSplash extends FlxSprite {
         animation.curAnim.frameRate = 24;
         clipRect = null;
         animation.finishCallback = (idkEither:Dynamic) -> {
-          visible = false;
+          die(tailEnd);
         }
         return;
       }
-      visible = false;
+      die(tailEnd);
     });
 
+  }
+
+  public function die(?end:Note = null):Void {
+    kill();
+    super.kill();
+    if (FlxG.state is PlayState) {
+      PlayState.instance.grpHoldSplashes.remove(this);
+    }
+    destroy();
+    super.destroy();
+    if (end != null) {
+      end.extraData['holdSplash'] = null;
+    }
   }
 
 }
