d391218b770e0fc480269c6c4b4bd9b0b9e7e928
Pumpsuki <71671417+PartnerCapPikachu@users.noreply.github.com>
2024-06-16 12:02:05 -0400

    Update Controls.hx

--- source/backend/Controls.hx
+++ source/backend/Controls.hx
@@ -77,9 +77,21 @@ class Controls
 	public var BACK(get, never):Bool;
 	public var PAUSE(get, never):Bool;
 	public var RESET(get, never):Bool;
-	private function get_ACCEPT() return justPressed('accept');
+	private function get_ACCEPT() {
+		var accepted:Bool = justPressed('accept');
+		if (FlxG.keys.justPressed.ENTER && accepted) {
+			accepted = accepted && !FlxG.keys.pressed.ALT;
+		}
+		return accepted;
+	}
 	private function get_BACK() return justPressed('back');
-	private function get_PAUSE() return justPressed('pause');
+	private function get_PAUSE() {
+		var paused:Bool = justPressed('pause');
+		if (FlxG.keys.justPressed.ENTER && paused) {
+			paused = paused && !FlxG.keys.pressed.ALT;
+		}
+		return paused;
+	}
 	private function get_RESET() return justPressed('reset');
 
 	//Gamepad & Keyboard stuff
