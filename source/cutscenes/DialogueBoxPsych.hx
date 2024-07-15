0ab931bf9ee12a20765994bd6146ea0e73ed52cc
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-07-15 18:18:28 -0400

    Update DialogueBoxPsych.hx

--- source/cutscenes/DialogueBoxPsych.hx
+++ source/cutscenes/DialogueBoxPsych.hx
@@ -6,8 +6,6 @@ import openfl.utils.Assets;
 import objects.TypedAlphabet;
 import cutscenes.DialogueCharacter;
 
-// Gonna try to kind of make it compatible to Forever Engine,
-// love u Shubs no homo :flushedh4:
 typedef DialogueFile = {
 	var dialogue:Array<DialogueLine>;
 }
