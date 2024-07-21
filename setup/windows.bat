6aff2b7f5feddfb83d21368b2a5404859417c5c9
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-07-21 18:06:34 -0400

    lock git library versions (except flxanimate for now), testing something new with actions

--- setup/windows.bat
+++ setup/windows.bat
@@ -10,9 +10,9 @@ haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
 haxelib install SScript 17.1.618
 haxelib install tjson 1.4.0
-haxelib install hxvlc
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git hxvlc https://github.com/MAJigsaw77/hxvlc 70e7f5f3e76d526ac6fb8f0e6665efe7dfda589d
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 633fcc051399afed6781dd60cbf30ed8c3fe2c5a
+haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc 3538a1c2bb07b04208cd014220207f8173acdb21
 echo Finished!
 pause
