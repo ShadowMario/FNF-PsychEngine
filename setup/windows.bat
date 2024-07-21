0bdfa7d20c493611363c1babf16e1d5b341ca4d3
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-07-21 18:17:41 -0400

    instruct actions to compile base game by default so people who use actions have something to play

--- setup/windows.bat
+++ setup/windows.bat
@@ -14,5 +14,6 @@ haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git hxvlc https://github.com/MAJigsaw77/hxvlc 70e7f5f3e76d526ac6fb8f0e6665efe7dfda589d
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 633fcc051399afed6781dd60cbf30ed8c3fe2c5a
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc 3538a1c2bb07b04208cd014220207f8173acdb21
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis d5361037efa3a02c4ab20b5bd14ca11e7d00f519
 echo Finished!
 pause
