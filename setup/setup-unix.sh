0aebae4c7036cad788425ffdca5b602a3d3b348a
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-07-20 20:23:13 -0400

    oops

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -4,7 +4,7 @@
 # https://haxe.org/download/version/4.2.5/
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
-haxelib install flixel 5.3.1
+haxelib install flixel 5.6.1
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1
 haxelib install SScript 17.1.618
--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -3,14 +3,14 @@ color 0a
 cd ..
 @echo on
 echo Installing dependencies.
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-tools
-haxelib install SScript
-haxelib install hxCodec
-haxelib install tjson
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib install flixel 5.6.1
+haxelib install flixel-addons 3.2.2
+haxelib install flixel-tools 1.5.1
+haxelib install SScript 17.1.618
+haxelib install tjson 1.4.0
+haxelib install hxvlc
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
