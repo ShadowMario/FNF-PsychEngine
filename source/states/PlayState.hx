87f0441738234a9352977028782de133fbdd9c50
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-08-02 09:38:23 -0400

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -2932,7 +2932,7 @@ class PlayState extends MusicBeatState
 
 		note.wasGoodHit = true;
 
-		if (ClientPrefs.data.hitsoundVolume > 0 && !note.hitsoundDisabled)
+		if ((ClientPrefs.data.hitsoundVolume > 0 || note.hitsoundForce) && !note.hitsoundDisabled)
 			FlxG.sound.play(Paths.sound(note.hitsound), ClientPrefs.data.hitsoundVolume);
 
 		if(!note.hitCausesMiss) //Common notes
