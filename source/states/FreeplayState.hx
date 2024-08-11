b88bfdc3df427708c2d1f85014c1caba42489fb8
tposejank <81495861+tposejank@users.noreply.github.com>
2024-08-11 17:57:09 -0400

    some errors

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -67,7 +67,7 @@ class FreeplayState extends MusicBeatState
 			{
 				FlxTransitionableState.skipNextTransIn = true;
 				persistentUpdate = false;
-				MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+				MusicBeatState.switchState(new states.ErrorState(Language.getPhrase('freeplay_no_weeks_added', "NO WEEKS ADDED FOR FREEPLAY\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu."),
 					function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
 					function() MusicBeatState.switchState(new states.MainMenuState())));
 				return;
--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -60,7 +60,7 @@ class StoryMenuState extends MusicBeatState
 		{
 			FlxTransitionableState.skipNextTransIn = true;
 			persistentUpdate = false;
-			MusicBeatState.switchState(new states.ErrorState("NO WEEKS ADDED FOR STORY MODE\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu.",
+			MusicBeatState.switchState(new states.ErrorState(Language.getPhrase('story_mode_no_weeks_added', "NO WEEKS ADDED FOR STORY MODE\n\nPress ACCEPT to go to the Week Editor Menu.\nPress BACK to return to Main Menu."),
 				function() MusicBeatState.switchState(new states.editors.WeekEditorState()),
 				function() MusicBeatState.switchState(new states.MainMenuState())));
 			return;
