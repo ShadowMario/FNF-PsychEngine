c9d13b2fcb045abbd8b8a3bb64ff1f90cfa690f9
Lily <egzozu.be.bas@gmail.com>
2024-08-21 15:24:05 +0300

    thanks to leer

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -1637,7 +1637,7 @@ class FunkinLua {
 			return;
 		}
 
-		if (Type.typeof(data) == TFunction) {
+		if (Reflect.isFunction(data)) {
 			Lua_helper.add_callback(lua, variable, data);
 			return;
 		}
