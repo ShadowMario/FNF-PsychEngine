272f05e70eb510a18e42ef770f78c6db0ec4fa2d
LarryFrosty <80482125+LarryFrosty@users.noreply.github.com>
2024-08-23 13:37:00 +0800

    revert to pre 1.0
    should make github think im not changing this part of the code

--- source/psychlua/LuaUtils.hx
+++ source/psychlua/LuaUtils.hx
@@ -497,12 +497,9 @@ class LuaUtils
 
 	public static function cameraFromString(cam:String):FlxCamera {
 		switch(cam.toLowerCase()) {
-			case 'camgame' | 'game': return PlayState.instance.camGame;
 			case 'camhud' | 'hud': return PlayState.instance.camHUD;
 			case 'camother' | 'other': return PlayState.instance.camOther;
 		}
-		var camera:FlxCamera = MusicBeatState.getVariables().get(cam);
-		if (camera == null || !Std.isOfType(camera, FlxCamera)) camera = PlayState.instance.camGame;
-		return camera;
+		return PlayState.instance.camGame;
 	}
 }
