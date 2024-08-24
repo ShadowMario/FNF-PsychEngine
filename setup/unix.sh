8def6ffb02d27329793e4b426e336ea28eee5619
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-08-24 04:16:08 -0400

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -12,7 +12,7 @@ haxelib install flixel-addons 3.2.2
 haxelib install flixel-tools 1.5.1	
 haxelib install hscript-iris 1.0.2
 haxelib install tjson 1.4.0
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 05a3e4d1c9b31ae5fc3f394bb5c9c3c477d7e506
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 633fcc051399afed6781dd60cbf30ed8c3fe2c5a
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc 3538a1c2bb07b04208cd014220207f8173acdb21
 haxelib git hxvlc https://github.com/MAJigsaw77/hxvlc 70e7f5f3e76d526ac6fb8f0e6665efe7dfda589d
