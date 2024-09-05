84aae8d885f561008c835159f1dc6ea1595be793
LarryFrosty <80482125+LarryFrosty@users.noreply.github.com>
2024-09-05 23:39:29 +0800

    Move script rating variables

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -3444,10 +3444,10 @@ class PlayState extends MusicBeatState
 			}
 			fullComboFunction();
 		}
-		updateScore(badHit); // score will only update after rating is calculated, if it's a badHit, it shouldn't bounce
 		setOnScripts('rating', ratingPercent);
 		setOnScripts('ratingName', ratingName);
 		setOnScripts('ratingFC', ratingFC);
+		updateScore(badHit); // score will only update after rating is calculated, if it's a badHit, it shouldn't bounce
 	}
 
 	#if ACHIEVEMENTS_ALLOWED
