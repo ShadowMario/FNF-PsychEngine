51bdd09e36957cc0a9e91fdbd7f3f8b0ff2f0ca9
Oren <777.too.lucky.777@gmail.com>
2024-09-17 10:23:53 +0900

    GitHub Automatic Builds Fix

--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -5,15 +5,16 @@ cd ..
 echo Installing dependencies.
 haxelib install lime
 haxelib install openfl
-haxelib install flixel
+haxelib install flixel 5.6.2
+haxelib set flixel 5.6.2
 haxelib install flixel-addons
 haxelib install flixel-ui
 haxelib install flixel-tools
-haxelib install SScript
 haxelib install hxCodec
 haxelib install tjson
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git SScript https://github.com/Oren-LuckyJackpots/SScript-For-Psych 8.1.6
 echo Finished!
 pause
