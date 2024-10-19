46be73b1b228b7605fc2c7aeba779a3fa8304227
LarryFrosty <80482125+LarryFrosty@users.noreply.github.com>
2024-10-20 04:44:57 +0800

    Why the fuck

--- source/psychlua/LuaUtils.hx
+++ source/psychlua/LuaUtils.hx
@@ -256,7 +256,7 @@ class LuaUtils
 			
 			default:
 				var obj:Dynamic = MusicBeatState.getVariables().get(objectName);
-				if(obj == null) obj = getVarInArray(MusicBeatState.getState(), objectName, allowMaps);
+				if(obj == null) obj = getVarInArray(getTargetInstance(), objectName, allowMaps);
 				return obj;
 		}
 	}
