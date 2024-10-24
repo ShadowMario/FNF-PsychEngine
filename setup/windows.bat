40749a5b38a0aa352522814e3fdaf1c741084efe
ShadowMario <44785097+ShadowMario@users.noreply.github.com>
2024-10-24 12:35:15 -0300

    Changed hxvlc version in setup files

--- setup/windows.bat
+++ setup/windows.bat
@@ -12,7 +12,7 @@ haxelib install flixel-tools 1.5.1
 haxelib install hscript-iris 1.1.0
 haxelib install tjson 1.4.0
 haxelib install hxdiscord_rpc 1.2.4
-haxelib install hxvlc 1.8.2
+haxelib install hxvlc 1.9.2
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
