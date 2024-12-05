179b7a6f04b0a645eae5724377c2d91891b38fab
DM-kun <86197998+DMMaster636@users.noreply.github.com>
2024-12-05 21:47:17 +0100

    Let's not forget EditorPlayState

--- source/states/editors/content/EditorPlayState.hx
+++ source/states/editors/content/EditorPlayState.hx
@@ -347,6 +347,12 @@ class EditorPlayState extends MusicBeatSubstate
 				for (evilNote in unspawnNotes) {
 					var matches: Bool = note.noteData == evilNote.noteData && note.mustPress == evilNote.mustPress && note.noteType == evilNote.noteType;
 					if (matches && Math.abs(note.strumTime - evilNote.strumTime) == 0.0) {
+						if (evilNote.tail.length > 0)
+							for (tail in evilNote.tail)
+							{
+								tail.destroy();
+								unspawnNotes.remove(tail);
+							}
 						evilNote.destroy();
 						unspawnNotes.remove(evilNote);
 						//continue;
