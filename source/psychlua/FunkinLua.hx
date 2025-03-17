c374b4d5f6927ea0670631659d4e665c4662d230
PringleKitten <wie054024@gmail.com>
2025-03-17 16:25:53 -0500

    playbackRate sync and more offset
    Sync all tweens / timers with playbackRate (There may be a better way to do it?)
    Let users use offsets up to 1000 cause why not, some bluetooth devices(mine) need more than 500 for perfect sync.

--- source/psychlua/FunkinLua.hx
+++ source/psychlua/FunkinLua.hx
@@ -49,6 +49,7 @@ class FunkinLua {
 	public var scriptName:String = '';
 	public var modFolder:String = null;
 	public var closed:Bool = false;
+	public var divideVal:Float = 0;
 
 	#if HSCRIPT_ALLOWED
 	public var hscript:HScript = null;
@@ -75,6 +76,8 @@ class FunkinLua {
 		if(myFolder[0] + '/' == Paths.mods() && (Mods.currentModDirectory == myFolder[1] || Mods.getGlobalMods().contains(myFolder[1]))) //is inside mods folder
 			this.modFolder = myFolder[1];
 		#end
+
+		divideVal = game.playbackRate;
 
 		// Lua shit
 		set('Function_StopLua', LuaUtils.Function_StopLua);
@@ -500,6 +503,7 @@ class FunkinLua {
 		// gay ass tweens
 		Lua_helper.add_callback(lua, "startTween", function(tag:String, vars:String, values:Any = null, duration:Float, ?options:Any = null) {
 			var penisExam:Dynamic = LuaUtils.tweenPrepare(tag, vars);
+			duration = duration/divideVal;
 			if(penisExam != null)
 			{
 				if(values != null)
@@ -602,18 +606,23 @@ class FunkinLua {
 
 		//Tween shit, but for strums
 		Lua_helper.add_callback(lua, "noteTweenX", function(tag:String, note:Int, value:Dynamic, duration:Float, ?ease:String = 'linear') {
+			duration = duration/divideVal;
 			return noteTweenFunction(tag, note, {x: value}, duration, ease);
 		});
 		Lua_helper.add_callback(lua, "noteTweenY", function(tag:String, note:Int, value:Dynamic, duration:Float, ?ease:String = 'linear') {
+			duration = duration/divideVal;
 			return noteTweenFunction(tag, note, {y: value}, duration, ease);
 		});
 		Lua_helper.add_callback(lua, "noteTweenAngle", function(tag:String, note:Int, value:Dynamic, duration:Float, ?ease:String = 'linear') {
+			duration = duration/divideVal;
 			return noteTweenFunction(tag, note, {angle: value}, duration, ease);
 		});
 		Lua_helper.add_callback(lua, "noteTweenAlpha", function(tag:String, note:Int, value:Dynamic, duration:Float, ?ease:String = 'linear') {
+			duration = duration/divideVal;
 			return noteTweenFunction(tag, note, {alpha: value}, duration, ease);
 		});
 		Lua_helper.add_callback(lua, "noteTweenDirection", function(tag:String, note:Int, value:Dynamic, duration:Float, ?ease:String = 'linear') {
+			duration = duration/divideVal;
 			return noteTweenFunction(tag, note, {direction: value}, duration, ease);
 		});
 		Lua_helper.add_callback(lua, "mouseClicked", function(?button:String = 'left') {
@@ -658,6 +667,7 @@ class FunkinLua {
 			
 			var originalTag:String = tag;
 			tag = LuaUtils.formatVariable('timer_$tag');
+			time = time/divideVal;
 			variables.set(tag, new FlxTimer().start(time, function(tmr:FlxTimer)
 			{
 				if(tmr.finished) variables.remove(tag);
@@ -835,13 +845,16 @@ class FunkinLua {
 		Lua_helper.add_callback(lua, "getCameraFollowY", () -> game.camFollow.y);
 
 		Lua_helper.add_callback(lua, "cameraShake", function(camera:String, intensity:Float, duration:Float) {
+			duration = duration/divideVal;
 			LuaUtils.cameraFromString(camera).shake(intensity, duration);
 		});
 
 		Lua_helper.add_callback(lua, "cameraFlash", function(camera:String, color:String, duration:Float,forced:Bool) {
+			duration = duration/divideVal;
 			LuaUtils.cameraFromString(camera).flash(CoolUtil.colorFromString(color), duration, null, forced);
 		});
 		Lua_helper.add_callback(lua, "cameraFade", function(camera:String, color:String, duration:Float, forced:Bool, ?fadeOut:Bool = false) {
+			duration = duration/divideVal;
 			LuaUtils.cameraFromString(camera).fade(CoolUtil.colorFromString(color), duration, fadeOut, null, forced);
 		});
 		Lua_helper.add_callback(lua, "setRatingPercent", function(value:Float) {
@@ -1382,6 +1395,7 @@ class FunkinLua {
 			}
 		});
 		Lua_helper.add_callback(lua, "soundFadeIn", function(tag:String, duration:Float, fromValue:Float = 0, toValue:Float = 1) {
+			duration = duration/divideVal;
 			if(tag == null || tag.length < 1)
 			{
 				if(FlxG.sound.music != null)
@@ -1396,6 +1410,7 @@ class FunkinLua {
 			}
 		});
 		Lua_helper.add_callback(lua, "soundFadeOut", function(tag:String, duration:Float, toValue:Float = 0) {
+			duration = duration/divideVal;
 			if(tag == null || tag.length < 1)
 			{
 				if(FlxG.sound.music != null)
@@ -1678,6 +1693,7 @@ class FunkinLua {
 	{
 		var target:Dynamic = LuaUtils.tweenPrepare(tag, vars);
 		var variables = MusicBeatState.getVariables();
+		duration = duration/divideVal;
 		if(target != null)
 		{
 			if(tag != null)
@@ -1705,6 +1721,8 @@ class FunkinLua {
 
 		var strumNote:StrumNote = PlayState.instance.strumLineNotes.members[note % PlayState.instance.strumLineNotes.length];
 		if(strumNote == null) return null;
+
+		duration = duration/divideVal;
 
 		if(tag != null)
 		{
